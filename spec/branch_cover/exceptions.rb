e9c8440b77657c0dee1365b4ea11abe5d02c478e
Marc-Andre Lafortune <github@marc-andre.ca>
2017-09-07 19:38:54 -0400

    Improve exceptions specs

--- spec/branch_cover/exceptions.rb
+++ spec/branch_cover/exceptions.rb
@@ -3,7 +3,66 @@
 
     begin
       raise
+    rescue
+    end
+
+    begin
+      raise
       "foo"
 #>X
     rescue
     end
+
+#### Without raise
+
+    begin
+      :dont_raise
+    rescue
+#>X
+    end
+
+### With exception list, but no assignment
+
+    begin
+      raise
+      "foo"
+#>X
+    rescue NotImplementedError, TypeError
+#>  xxxxxx
+    rescue Exception
+      "here"
+    rescue SyntaxError, TypeError
+#>X
+      "not here"
+#>X
+    end
+
+#### Without raise
+    begin
+      :dont_raise
+    rescue NotImplementedError
+#>X
+    end
+
+### With assigned exception list
+    begin
+      raise
+      "foo"
+#>X
+    rescue NotImplementedError, TypeError => foo
+#>  xxxxxx                                xxxxxx
+    rescue Exception => bar
+      "here"
+    rescue SyntaxError, TypeError => baz
+#>X
+      "not here"
+#>X
+    end
+
+#### Without raise
+    begin
+      :dont_raise
+    rescue NotImplementedError => foo
+#>X
+    end
+
