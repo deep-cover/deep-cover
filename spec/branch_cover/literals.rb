4af96954b057171d8ddaefc85ba56b87e7d155cb
Marc-Andre Lafortune <github@marc-andre.ca>
2017-09-07 18:59:15 -0400

    Literals now pass

--- spec/branch_cover/literals.rb
+++ spec/branch_cover/literals.rb
@@ -3,6 +3,7 @@
     1.0...4.2
     1..(2+2)
     raise..42 rescue nil
+#>         xx
     'a'..'z'
     a = 42
     1..a
@@ -16,7 +17,8 @@
 ### Symbols
     :hello
     :"he#{'l'*2}o"
-    :"h#{3}l#{raise}o#{'2'}#{:x}" rescue nil
+    :"h#{3}l#{raise}o#{'2'}-#{:x}" rescue nil
+#>                   xxxxxx xxxxx
     %s[hello]
     %i[hello world]
 ### Strings
@@ -26,6 +28,7 @@
     /regexp/
     /re#{'g'}exp/i
     /re#{raise}g#{'e'}p#{:x}/i rescue nil
+#>              xxxxxx xxxxx
     %r[regexp]
 ### Array
     [1, 2, 3]
@@ -33,10 +36,13 @@
     [1, *nil?, 3]
     [1, *[2], 3]
     [1, raise, *nil?, 3] rescue nil
+#>             xxxxx  x
     [1, *raise, 3] rescue nil
+#>              x
 ### Hash
     {:a => 1, :b => 2}
     {a: 1, b: 2}
     {a: 1, **{b: 2}}
     {nil? => 1, :b => 2}
     {a: raise, :b => 2} rescue nil
+#>             xx xx x
