8e5660d3a54b573bbcaf0374adbf770d3c73dd5b
Marc-Andre Lafortune <github@marc-andre.ca>
2017-09-09 16:41:16 -0400

    Add sanity proof check

--- lib/deep_cover/node.rb
+++ lib/deep_cover/node.rb
@@ -1,3 +1,15 @@
 require_relative 'node/base'
 require_relative_dir 'node_behavior'
 require_relative_dir 'node'
+
+module DeepCover
+  Node.constants.each do |name|
+    klass = Node.const_get(name)
+    if klass < Node
+      if klass.const_defined?(:CHILDREN) &&
+          klass.instance_method(:full_runs).owner == Node
+        warn "Class #{klass} has children but has not refined full_runs"
+      end
+    end
+  end
+end
