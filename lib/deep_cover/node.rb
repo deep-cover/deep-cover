f970da142b001e0b36253aaab5c0b091067b74b8
Marc-Andre Lafortune <github@marc-andre.ca>
2017-09-11 02:24:28 -0400

    Add warning for containers

--- lib/deep_cover/node.rb
+++ lib/deep_cover/node.rb
@@ -6,9 +6,16 @@ module DeepCover
   Node.constants.each do |name|
     klass = Node.const_get(name)
     if klass < Node
-      if klass.const_defined?(:CHILDREN) &&
-          klass.instance_method(:full_runs).owner == Node
-        warn "Class #{klass} has children but has not refined full_runs"
+      if klass.const_defined?(:CHILDREN)
+        if klass <  NodeBehavior::CoverWithNextInstruction
+          indices = klass::CHILDREN.values
+          if indices.size > 1 || indices[0].is_a?(Range)
+            warn "Class #{klass} has many children but is using CoverWithNextInstruction"
+          end
+        end
+        if klass.instance_method(:full_runs).owner == Node
+          warn "Class #{klass} has children but has not refined full_runs"
+        end
       end
     end
   end
