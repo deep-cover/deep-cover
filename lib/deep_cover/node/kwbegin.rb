9d89e2df93c8a960b89e3955f12a34f5bb3ba53e
Marc-Andre Lafortune <github@marc-andre.ca>
2017-09-15 15:35:58 -0400

    - redundant code

--- lib/deep_cover/node/kwbegin.rb
+++ lib/deep_cover/node/kwbegin.rb
@@ -1,11 +1,6 @@
 module DeepCover
   class Node
     class Kwbegin < Node
-      def flow_completion_count
-        last = children.last
-        return last.flow_completion_count if last
-        super
-      end
     end
   end
 end
