e1ae8cf13b33e4fe9acbb957c657caf9b0b5ca2d
Marc-Andre Lafortune <github@marc-andre.ca>
2017-09-15 19:51:48 -0400

    Use tracker code in Root

--- lib/deep_cover/ast_root.rb
+++ lib/deep_cover/ast_root.rb
@@ -1,20 +1,29 @@
 require 'backports/2.1.0/enumerable/to_h'
+require_relative 'has_tracker'
 
 module DeepCover
   class AstRoot
+    module FreezingIsPainful
+      def assign_properties
+      end
+    end
+    include FreezingIsPainful
+    include HasTracker
+
+    has_tracker :root
     attr_reader :file_coverage, :nb
 
     def initialize(file_coverage)
       @file_coverage = file_coverage
-      @nb = file_coverage.create_node_nb
+      assign_properties
     end
 
     def child_flow_entry_count(_child)
-      file_coverage.cover.fetch(nb*2)
+      root_tracker_hits
     end
 
     def child_prefix(_child)
-      "(($_cov[#{file_coverage.nb}][#{nb*2}] += 1;"
+      "((#{root_tracker_source};"
     end
 
     def child_suffix(_child)
