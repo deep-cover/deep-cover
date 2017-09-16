296ade752cf2bf6e0d796dd740902f65d1265718
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-09-16 10:45:59 -0400

    Basic spec for bin/cov not to fail

---
+++ spec/bin_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+
+RSpec.describe 'bin/cov' do
+  it 'run `bin/cover if 0` successfully' do
+    command_success = system({"DISABLE_PRY" => "1"}, "bin/cov if 0", in: File::NULL, out: File::NULL, err: File::NULL)
+    command_success.should be true
+  end
+end
