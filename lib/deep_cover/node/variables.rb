e47df57efe27ac83ee0d232816ad521757f169a7
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-09-15 23:05:02 -0400

    Support for Constants assignations

--- lib/deep_cover/node/variables.rb
+++ lib/deep_cover/node/variables.rb
@@ -24,5 +24,16 @@ module DeepCover
         value.flow_completion_count
       end
     end
+
+    class Casgn < Node
+      has_child cbase: [Cbase, Const, nil]
+      has_child var_name: Symbol
+      has_child value: [Node, nil]
+
+      def execution_count
+        return super unless value
+        value.flow_completion_count
+      end
+    end
   end
 end
--- spec/branch_cover/constants.rb
+++ spec/branch_cover/constants.rb
@@ -1,15 +1,19 @@
 ### Global
 
     ::String
+    ::SpecConstantsSampleGlobalAssignation = 1
 
 ### Unscoped
 
     String
+    SpecConstantsSampleUnscopedAssignation = 1
 
 ### Scoped
 
     Float::INFINITY
     ::Float::INFINITY
+    DeepCover::SpecConstantsSampleScopedAssignation = 1
+    ::DeepCover::SpecConstantsSampleScopedAssignation = 1
 
 ### Undefined
 #### Scoped
@@ -17,6 +21,12 @@
     begin
       Float::INFINITY::Nope::Neither
 #>                         xxxxxxxxx
+    rescue
+    end
+
+    begin
+      DeepCover::SpecConstantsSampleMissing::Assignation = 1
+#>                                         xxxxxxxxxxxxx x x
     rescue
     end
 
