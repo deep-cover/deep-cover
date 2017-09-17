0b96e6f87d3c7a85e7eaf647e52f07813d17f540
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-09-17 19:21:25 -0400

    custom_require_spec prints everything when there is an error

--- spec/custom_requirer_spec.rb
+++ spec/custom_requirer_spec.rb
@@ -19,6 +19,87 @@ module DeepCover
       end
     end
 
+    matcher :actually_require do |expected_executed_file|
+      match do |require_path|
+        @result = {}
+        @executed_file = {}
+        @loaded_features = {}
+
+        set_expectations(expected_executed_file)
+
+        run_ruby_require(require_path)
+        run_custom_require(require_path)
+
+        %w(result executed_file loaded_features).all? do |value_name|
+          values = instance_variable_get("@#{value_name}").values
+          values.all?{|v| v == values[0] }
+        end
+      end
+
+      failure_message do |require_path|
+        output = "Unexpected effects of `require #{require_path.inspect}`:\n"
+        output << "Return value:\n"
+        output << build_output(@result)
+        output << "\n\nExecuted file:\n"
+        output << build_output(@executed_file)
+        output << "\n\nLoaded features:\n"
+        output << build_output(@loaded_features)
+        output
+      end
+
+      def set_expectations(expected_executed_file)
+        case expected_executed_file
+        when :not_found
+          @executed_file[:expected] = nil
+          @result[:expected] = :not_found
+        when false
+          @executed_file[:expected] = nil
+          @result[:expected] = false
+        else
+          @executed_file[:expected] = expected_executed_file
+          @result[:expected] = true
+        end
+        @loaded_features[:expected] = requirer.loaded_features.dup
+        @loaded_features[:expected] += [from_root(expected_executed_file)] if expected_executed_file.is_a?(String)
+      end
+
+      # Must be run before run_custom_require
+      def run_ruby_require(require_path)
+        $last_test_tree_file_executed = nil
+        # Test ruby require
+        begin
+          tmp_load_path = $LOAD_PATH.dup
+          $LOAD_PATH[0..-1] = requirer.load_path
+          tmp_loaded_features = $LOADED_FEATURES.dup
+          $LOADED_FEATURES[0..-1] = requirer.loaded_features
+          @result[:ruby] = require(require_path)
+        rescue LoadError
+          @result[:ruby] = :not_found
+        ensure
+          @loaded_features[:ruby] = $LOADED_FEATURES.dup
+          $LOAD_PATH[0..-1] = tmp_load_path
+          $LOADED_FEATURES[0..-1] = tmp_loaded_features
+        end
+        @executed_file[:ruby] = $last_test_tree_file_executed
+      end
+
+      def run_custom_require(require_path)
+        $last_test_tree_file_executed = nil
+        @result[:custom] = requirer.require(require_path)
+        @loaded_features[:custom] = requirer.loaded_features.dup
+        @executed_file[:custom] = $last_test_tree_file_executed
+      end
+
+      def build_output(data_hash)
+        keys = [:expected, :ruby, :custom]
+        max_ley_length = keys.map(&:size).max
+        keys.map do |key|
+          indent = " " * (max_ley_length - key.size)
+          "  #{key}:#{indent} #{data_hash[key].inspect}"
+        end.join("\n")
+      end
+    end
+
     def add_load_path(path)
       requirer.load_path << from_root(path)
     end
@@ -29,49 +110,6 @@ module DeepCover
 
     def from_root(path)
       File.absolute_path(path, root)
-    end
-
-    def compare_require(require_path, expected_executed_file)
-      expected_executed_absolute_path = from_root(expected_executed_file) if expected_executed_file.is_a?(String)
-      init_loaded_features = requirer.loaded_features.dup
-
-      $last_test_tree_file_executed = nil
-      custom_result = requirer.require(require_path)
-      custom_executed_file = $last_test_tree_file_executed
-
-      $last_test_tree_file_executed = nil
-      begin
-        tmp_load_path = $LOAD_PATH.dup
-        $LOAD_PATH[0..-1] = requirer.load_path
-        tmp_loaded_features = $LOADED_FEATURES.dup
-        $LOADED_FEATURES[0..-1] = init_loaded_features
-        ruby_result = require(require_path)
-      rescue LoadError
-        ruby_result = :not_found
-      ensure
-        ruby_loaded_features_after = $LOADED_FEATURES.dup
-        $LOAD_PATH[0..-1] = tmp_load_path
-        $LOADED_FEATURES[0..-1] = tmp_loaded_features
-      end
-      ruby_executed_file = $last_test_tree_file_executed
-      $last_test_tree_file_executed = nil
-
-      if expected_executed_file == :not_found
-        expected_executed_file = nil
-        expected_result = :not_found
-      elsif expected_executed_file == false
-        expected_executed_file = nil
-        expected_result = false
-      else
-        expected_result = true
-      end
-
-      custom_result.should eq(ruby_result).and eq(expected_result)
-
-      custom_executed_file.should eq(ruby_executed_file).and eq(expected_executed_file)
-
-      expected_loaded_features = init_loaded_features + [expected_executed_absolute_path].compact
-      requirer.loaded_features.should eq(ruby_loaded_features_after).and eq(expected_loaded_features)
     end
 
     describe "#require" do
@@ -87,7 +125,7 @@ module DeepCover
                          one/two/test.rb
                          one/two/three/test.rb)
 
-        compare_require('./test', 'one/two/test.rb')
+        './test'.should actually_require('one/two/test.rb')
       end
 
       it "a ./path ignores the load_path" do
@@ -95,7 +133,7 @@ module DeepCover
                      one/two/three/test.rb)
 
         add_load_path 'one/two'
-        compare_require('./test', :not_found)
+        './test'.should actually_require(:not_found)
       end
 
       it "handles a ../path relatively to current work dir" do
@@ -104,7 +142,7 @@ module DeepCover
                          one/two/test.rb
                          one/two/three/test.rb)
 
-        compare_require('../test', 'one/test.rb')
+        '../test'.should actually_require('one/test.rb')
       end
 
       it "a ../path ignores the load_path" do
@@ -112,7 +150,7 @@ module DeepCover
                      one/two/three/test.rb)
         add_load_path 'one/two/three'
 
-        compare_require('../test', :not_found)
+        '../test'.should actually_require(:not_found)
       end
 
       it "a /../ in a path after a name will go to the parent" do
@@ -121,7 +159,7 @@ module DeepCover
                      one/two/three/test.rb)
         add_load_path 'one'
 
-        compare_require('two/../test', 'one/test.rb')
+        'two/../test'.should actually_require('one/test.rb')
       end
 
       it "a /./ in a path after a name will be ignored " do
@@ -130,7 +168,7 @@ module DeepCover
                      one/two/three/test.rb)
         add_load_path 'one'
 
-        compare_require('two/./test', 'one/two/test.rb')
+        'two/./test'.should actually_require('one/two/test.rb')
       end
 
       it "Can go to parent of load_path with multiple /../" do
@@ -139,7 +177,7 @@ module DeepCover
                      one/two/three/test.rb)
         add_load_path 'one/two'
 
-        compare_require('three/../../test', 'one/test.rb')
+        'three/../../test'.should actually_require('one/test.rb')
       end
 
       it "finds files with directories in the required path" do
@@ -148,7 +186,7 @@ module DeepCover
                      one/two/three/test.rb)
         add_load_path 'one'
 
-        compare_require('two/three/test', 'one/two/three/test.rb')
+        'two/three/test'.should actually_require('one/two/three/test.rb')
       end
 
       it "uses the matching file from the the first matching load_path" do
@@ -159,7 +197,7 @@ module DeepCover
         add_load_path 'one/two'
         add_load_path 'one/two/three'
 
-        compare_require('test', 'one/test.rb')
+        'test'.should actually_require('one/test.rb')
       end
 
       # TOMA: Is that a useless test?
@@ -171,7 +209,7 @@ module DeepCover
                      one/two/three/test.rb)
         add_load_path 'one/two'
 
-        compare_require('test', 'one/two/test.rb')
+        'test'.should actually_require('one/two/test.rb')
       end
 
       # TOMA: Should this be split into multiple test?
@@ -187,8 +225,8 @@ module DeepCover
         add_load_path 'one/two'
         add_load_path 'one/two/three'
 
-        compare_require('test', 'one/test.rb')
-        compare_require('test', false)
+        'test'.should actually_require('one/test.rb')
+        'test'.should actually_require(false)
       end
 
       it "doesn't execute a file that was already required through another load_path" do
@@ -196,10 +234,10 @@ module DeepCover
                      one/two/test.rb
                      one/two/three/test.rb)
         add_load_path 'one/two'
-        compare_require('test', 'one/two/test.rb')
+        'test'.should actually_require('one/two/test.rb')
 
         add_load_path 'one'
-        compare_require('two/test', false)
+        'two/test'.should actually_require(false)
       end
 
       it "doesn't find from parent or subdir of the load_path" do
@@ -207,7 +245,7 @@ module DeepCover
                      one/two/three/test.rb)
         add_load_path 'one/two'
 
-        compare_require('test', :not_found)
+        'test'.should actually_require(:not_found)
       end
 
       it "regular path ignores current work dir" do
@@ -216,7 +254,7 @@ module DeepCover
                          one/two/test.rb
                          one/two/three/test.rb)
 
-        compare_require('test', :not_found)
+        'test'.should actually_require(:not_found)
       end
 
       it "ignores a not .rb file when path includes .rb" do
@@ -225,7 +263,7 @@ module DeepCover
                      one/two/three/test.rb)
         add_load_path 'one/two'
 
-        compare_require('test', :not_found)
+        'test'.should actually_require(:not_found)
       end
 
       it "ignores a not .rb file when path is without .rb" do
@@ -234,7 +272,7 @@ module DeepCover
                      one/two/three/test.rb)
         add_load_path 'one/two'
 
-        compare_require('test', :not_found)
+        'test'.should actually_require(:not_found)
       end
     end
 
