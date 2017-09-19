a08bd62f5b5434fc0a84fc54f3a010ef2c5c56b6
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-09-19 08:23:43 -0400

    Typo

--- spec/bin_spec.rb
+++ spec/bin_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 
 RSpec.describe 'bin/cov' do
-  it 'run `bin/cover if 0` successfully' do
+  it 'run `bin/cov if 0` successfully' do
     command_success = system({"DISABLE_PRY" => "1"}, "bin/cov if 0", in: File::NULL, out: File::NULL, err: File::NULL)
     command_success.should be true
   end
