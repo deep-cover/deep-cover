a364a1567ee85be260016e4579604d433f7e8a89
Marc-Andre Lafortune <github@marc-andre.ca>
2017-09-21 01:06:45 -0400

    assignation -> assignment

--- lib/deep_cover/node/loops.rb
+++ lib/deep_cover/node/loops.rb
@@ -2,7 +2,7 @@ module DeepCover
   class Node
     class For < Node
       has_tracker :body
-      has_child assignations: [Mlhs, VariableAssignment], flow_entry_count: -> { body.flow_entry_count if body }
+      has_child assignments: [Mlhs, VariableAssignment], flow_entry_count: -> { body.flow_entry_count if body }
       has_child iterable: [Node], flow_entry_count: -> { flow_entry_count }
       has_child body: [Node, nil], flow_entry_count: :body_tracker_hits,
         rewrite: '((%{body_tracker}; %{node}))'
