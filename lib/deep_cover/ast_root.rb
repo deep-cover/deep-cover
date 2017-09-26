2e4a690b9d10f6154ea0d1552e22806c002c6493
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-09-26 09:12:39 -0400

    AstRoot has children instead of main_node

--- lib/deep_cover/ast_root.rb
+++ lib/deep_cover/ast_root.rb
@@ -9,16 +9,13 @@ module DeepCover
     has_child main: Node, flow_entry_count: :root_tracker_hits, rewrite: -> {
       "#{covered_code.trackers_setup_source};%{root_tracker};%{node}"
     }
-    attr_reader :covered_code
+    attr_reader :covered_code, :children
 
     def initialize(child_ast, covered_code)
       @covered_code = covered_code
-      @main_node = self.class.augment(child_ast, covered_code, self)
+      @children = [child_ast] # Temporary set for augment
+      @children = [self.class.augment(child_ast, covered_code, self)]
       super()
-    end
-
-    def children
-      [@main_node]
     end
   end
 end
