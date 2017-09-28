4b3c37b251deb203050d921ace0ade993c0e5480
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-09-27 21:38:53 -0400

    Spec to check branch_cover samples to match builtin

--- spec/deep_cover_spec.rb
+++ spec/deep_cover_spec.rb
@@ -1,27 +1,7 @@
 require "spec_helper"
 
-RSpec::Matchers.define :match_coverage do
-  match do |fn|
-    @our = DeepCover::Tools.our_coverage(fn)
-    @builtin = DeepCover::Tools.builtin_coverage(fn)
-    @our.zip(@builtin).all? do |us, ruby|
-      # accept us > ruby > 0; can happen for example with `def foo(arg = this_can_run_many_times)`
-      cmp = us <=> ruby
-      cmp && (cmp == 0 || (cmp > 0 && ruby > 0)) # either equal, or us > ruby > 1
-    end
-  end
-  failure_message do |fn|
-    DeepCover::Tools.format(@builtin, @our, filename: fn).join
-  end
-end
 
 RSpec.describe DeepCover do
-  Dir.glob('./spec/samples/*.rb').each do |fn|
-    it "returns the same coverage for '#{File.basename(fn, '.rb')}' as the builtin one" do
-      File.absolute_path(fn).should match_coverage
-    end
-  end
-
   it "Can create a CoveredCode with empty source" do
     expect { DeepCover::CoveredCode.new(source: '') }.not_to raise_error
   end
---
+++ spec/line_coverage_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+require "tempfile"
+
+RSpec::Matchers.define :match_coverage do |fn, lines, lineno|
+  match do
+    Tempfile.open(["#{File.basename(fn)}_#{lineno}_", '.rb']) do |tmp|
+      new_lines_added = lineno - 1
+      tmp.write("\n" * new_lines_added + lines.join)
+      tmp.close
+
+      @builtin = DeepCover::Tools.builtin_coverage(tmp.path)[new_lines_added..-1]
+      @our = DeepCover::Tools.our_coverage(tmp.path)[new_lines_added..-1]
+    end
+
+    @our.zip(@builtin).all? do |us, ruby|
+      # accept us > ruby > 0; can happen for example with `def foo(arg = this_can_run_many_times)`
+      cmp = us <=> ruby
+      cmp && (cmp == 0 || (cmp > 0 && ruby > 0)) # either equal, or us > ruby > 1
+    end
+  end
+  failure_message do
+    result = DeepCover::Tools.format(@builtin, @our, source: lines.join).join
+    "Builtin & DeepCover's line coverage should match\n#{result}"
+  end
+end
+
+RSpec.describe 'line coverage' do
+  each_code_examples('./spec/branch_cover/*.rb') do |fn, lines, lineno|
+    should match_coverage(fn, lines, lineno)
+  end
+end
