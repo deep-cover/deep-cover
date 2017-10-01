bca38a24878c58b4b8e380f0aff26480bdc7fde6
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-09-30 21:27:19 -0400

    Specs for some special code formats to compare with coverage_c

---
+++ spec/branch_cover/coverage_c.rb
@@ -0,0 +1,24 @@
+# These are unusual code patterns to compare the builtin line coverage with our own.
+# There may be overlap with other sample files. Too bad
+
+### Case with one-liner when
+    case 1
+    when "hi"
+      "not here"
+#>X
+    when 1; "here"
+    end
+
+### Multi-line literals
+    [
+        1,
+        2,
+        3,
+    ]
+
+    {
+        a: 3,
+        b: [
+            4,
+        ]
+    }
