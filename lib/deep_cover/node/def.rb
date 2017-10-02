6bda75536d6956b6a4418084da0887246332ec49
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-02 12:19:34 -0400

    Fix def and singleton def

--- lib/deep_cover/node/def.rb
+++ lib/deep_cover/node/def.rb
@@ -2,19 +2,27 @@ require_relative 'arguments'
 
 module DeepCover
   class Node::Def < Node
+    check_completion
     has_tracker :method_call
     has_child method_name: Symbol
     has_child signature: Args
     has_child body: [Node, nil], rewrite: '%{method_call_tracker};%{node};',
       flow_entry_count: :method_call_tracker_hits
-
-    alias_method :flow_completion_count, :flow_entry_count
+    def children_nodes_in_flow_order
+      []
+    end
   end
 
   class Node::Defs < Node
-    has_child singleton: Node
+    check_completion
+    has_tracker :method_call
+    has_child singleton: Node, rewrite: '(%{node})'
     has_child method_name: Symbol
     has_child signature: Args
-    has_child body: [Node, nil]
+    has_child body: [Node, nil], rewrite: '%{method_call_tracker};%{node};',
+      flow_entry_count: :method_call_tracker_hits
+    def children_nodes_in_flow_order
+      [singleton]
+    end
   end
 end
---
+++ spec/branch_cover/def.rb
@@ -0,0 +1,48 @@
+### Def
+#### Simple
+    module M; def foo; 42; end; end
+#>                     xx
+
+#### With args
+    module M; def bar(a, b, c=1, *rest, d: 2, **h); end; end
+#>                   ---------x------------x------
+
+#### Called
+    class C; def bar(a, b, c=1, *rest, d: 2, **h); 42; end; end; C.new.bar(1,2)
+
+#### Raising
+    module Frozen; freeze; def foo; end; 42; end rescue nil
+#>                                       xx
+
+### Singleton def
+#### Simple
+    o = {}; def o.some_method; 42; end
+#>                             xx
+
+#### With args
+    o = {}; def o.foo(a, b, c=1, *rest, d: 2, **h); end
+#>                   ---------x------------x------
+
+#### Called
+    o = {}; def o.foo(a, b, c=1, *rest, d: 2, **h); end; o.foo(1,2)
+
+#### of const
+    def Integer.some_method; end
+
+#### of self
+    def self.some_method; end
+
+#### of send
+    def dummy_method.some_method; end
+
+#### of send chain
+    def (Array.new).some_method; end
+
+#### with raising singleton
+    def does_not_exist.some_method; end rescue nil
+#>  xxx               xxxxxxxxxxxxxxxxx
+
+#### Raising
+    module Frozen; freeze; end;
+    (def Frozen.foo; end; 42) rescue nil
+#>                        xx
