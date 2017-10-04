d6bb2a142af0a302a723c119badd6a42f709c553
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-04 11:10:08 -0400

    + has_child_handler. Split has_child.

---
+++ lib/deep_cover/node/mixin/can_augment_children.rb
@@ -0,0 +1,56 @@
+module DeepCover
+  module Node::Mixin
+    module CanAugmentChildren
+      def self.included(base)
+        base.has_child_handler('remap_%{name}')
+        base.singleton_class.prepend ClassMethods
+      end
+
+      # Augment creates a covered node from the child_base_node.
+      # Caution: receiver is not fully constructed since it is also being augmented.
+      #          don't call `children`
+      def augment_children(child_base_nodes)
+        # Skip children that aren't node themselves (e.g. the `method` child of a :def node)
+        child_base_nodes.map.with_index do |child, child_index|
+          child_name = self.class.child_index_to_name(child_index, child_base_nodes.size) rescue binding.pry
+
+          klass = remap_child(child, child_name)
+          next child if !klass && !child.is_a?(Parser::AST::Node)
+
+          klass ||= self.class.factory(child.type, child_index)
+          klass.new(child, parent: self, index: child_index)
+        end
+      end
+      private :augment_children
+
+      module ClassMethods
+
+        # This handles the following shortcuts:
+        #   has_child foo: {type: NodeClass, ...}
+        # same as:
+        #   has_child foo: [], remap: {type: NodeClass, ...}
+        # same as:
+        #   has_child foo: [NodeClass, ...], remap: {type: NodeClass, ...}
+        #
+        def has_child(remap: nil, **h)
+          name, types = h.first
+          if types.is_a? Hash
+            raise "Use either remap or a hash as type but not both" if remap
+            remap = types
+            h[name] = types = []
+          end
+          if remap.is_a? Hash
+            type_map = remap
+            remap = -> (child) do
+              klass = type_map[child.type] if child.respond_to? :type
+              klass ||= type_map[child.class]
+              klass
+            end
+            types.concat(type_map.values).uniq!
+          end
+          super(**h, remap: remap)
+        end
+      end
+    end
+  end
+end
---
+++ lib/deep_cover/node/mixin/has_child_handler.rb
@@ -0,0 +1,61 @@
+module DeepCover
+  module Node::Mixin
+    module HasChildHandler
+      def self.included(base)
+        base.extend ClassMethods
+      end
+
+      def call_child_handler template, child, child_name = nil
+        child_name ||= self.class.child_index_to_name(child.index, children.size) rescue binding.pry
+        method_name = template % {name: child_name}
+        if respond_to?(method_name)
+          args = [child] unless method(method_name).arity == 0
+          answer = send(method_name, *args)
+        end
+        answer
+      end
+      private :call_child_handler
+
+      module ClassMethods
+        def has_child_handler(template)
+          child_method_name = template % {name: 'child'}
+          action = template.gsub /_?%{name}_?/, ''
+          const_name = "#{Misc.camelize(action)}Handler"
+          class_eval <<-end_eval, __FILE__, __LINE__
+            module #{const_name}                                     # module RewriteHandler
+              module ClassMethods                                    #   module ClassMethods
+                def has_child(#{action}: nil, **h)                   #     def has_child(rewrite: nil, **h)
+                  name, types = h.first                              #       name, types = h.first
+                  define_child_handler(#{template.inspect},          #       define_child_handler('rewrite_%{child}',
+                    name, #{action})                                 #         name, rewrite)
+                  super(**h)                                         #       super(**h)
+                end                                                  #     end
+              end                                                    #   end
+
+              def #{child_method_name}(child, name = nil)            #   def rewrite_child(child, child_name = nil)
+                call_child_handler(#{template.inspect}, child, name) #     call_child_handler('rewrite_%{child}', child, child_name)
+              end                                                    #   end
+            end                                                      # end
+            include #{const_name}                                    # include RewriteHandler
+            singleton_class.prepend #{const_name}::ClassMethods      # singleton_class.prepend RewriteHandler::ClassMethods
+          end_eval
+        end
+
+        def define_child_handler(template, name, method)
+          method_name = template % {name: name}
+          case method
+          when nil
+            # Nothing to do
+          when Symbol
+            alias_method method_name, method
+          when Proc
+            define_method(method_name, &method)
+          else
+            define_method(method_name) {|*| method }
+          end
+        end
+        private :define_child_handler
+      end
+    end
+  end
+end
