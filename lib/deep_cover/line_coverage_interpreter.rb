7f1d1572a298e6ddbe985aeb80478efe80b6b380
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-10-05 17:04:44 -0400

    Use each_node in LineCoverageInterpreter

--- lib/deep_cover/line_coverage_interpreter.rb
+++ lib/deep_cover/line_coverage_interpreter.rb
@@ -8,21 +8,16 @@ module DeepCover
 
     def generate_results
       line_hits = Array.new(@covered_code.nb_lines)
-      return line_hits unless @covered_code.covered_ast
-      apply_line_hits(@covered_code.covered_ast, line_hits)
-
-      line_hits
-    end
-
-    # TODO: use each_node in generate_results instead of manually iterating to the children
-    def apply_line_hits(node, line_hits)
-      if node.executable?
-        node.executed_locs.each do |loc|
-          lineno = loc.line - 1
-          line_hits[lineno] = [line_hits[lineno] || 0, node.execution_count].max
+      @covered_code.covered_ast.each_node do |node|
+        if node.executable?
+          node.executed_locs.each do |loc|
+            lineno = loc.line - 1
+            line_hits[lineno] = [line_hits[lineno] || 0, node.execution_count].max
+          end
         end
       end
-      node.children_nodes.each{|c| apply_line_hits(c, line_hits) }
+
+      line_hits
     end
   end
 end
