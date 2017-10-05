9df7f6ee18e799381093d4e16cee7c3d9156a046
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-10-04 21:15:23 -0400

    Add spec for some special send

--- spec/branch_cover/send.rb
+++ spec/branch_cover/send.rb
@@ -6,3 +6,24 @@
 
 ### Without outer parenthesis
     [].push 1, [].push(2), 3
+
+### [] and .[]
+    foo = []
+    foo[1]
+    foo.[](3)
+    foo.[] 3
+
+### []= and .[]=
+    foo = []
+    foo[1] = 2
+    foo.[]=(3, 4)
+    foo.[]= 3, 4
+
+### .()
+    a = proc {|o| o}
+    a.()
+    a.(3)
+
+### Multiple assignment
+    foo = []
+    foo[1], foo[2] = 1,2
