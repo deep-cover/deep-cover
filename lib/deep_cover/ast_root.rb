801f43b28ed018fbfe40c5dcd7b0d6bd16e9a51b
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-04 20:01:35 -0400

    Make AstRoot a full node

--- lib/deep_cover/ast_root.rb
+++ lib/deep_cover/ast_root.rb
@@ -1,21 +1,16 @@
 require_relative 'node'
 
 module DeepCover
-  class AstRoot
-    include Node::Mixin
-    include HasTracker
-    include HasChild
-
+  class AstRoot < Node
     has_tracker :root
     has_child main: Node, flow_entry_count: :root_tracker_hits, rewrite: -> {
       "#{covered_code.trackers_setup_source};%{root_tracker};%{node}"
     }
-    attr_reader :covered_code, :children
+    attr_reader :covered_code
 
     def initialize(child_ast, covered_code)
       @covered_code = covered_code
-      @children = augment_children([child_ast])
-      super()
+      super(nil, parent: nil, base_children: [child_ast])
     end
   end
 end
