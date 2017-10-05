d7a52415070eadbf4fad8017cce2ac97b50c6327
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-10-04 21:19:35 -0400

    Add spec for actual line coverage, only no files for now

--- spec/line_coverage_spec.rb
+++ spec/line_coverage_spec.rb
@@ -1,3 +1,51 @@
 require "spec_helper"
 require "tempfile"
 
+
+RSpec::Matchers.define :have_correct_line_coverage do |filename, lines, lineno|
+  match do
+    @our = DeepCover::Tools.our_coverage(lines.join, filename, lineno)
+    answers = DeepCover::Tools::parse_cov_comments_answers(lines)
+
+    errors = @our.zip(answers, lines).each_with_index.reject do |(cov, comment_answer, line), _i|
+      expected_result?(cov, line, comment_answer)
+    end
+    @errors = errors.map{|_, i| i + lineno}
+    @errors.empty?
+  end
+  failure_message do |fn|
+    result = DeepCover::Tools.format(@our, source: lines.join, lineno: lineno, bad_linenos: @errors).join
+    "Line coverage does not match on lines #{@errors.join(', ')}\n#{result}"
+  end
+
+  def expected_result?(cov, line, comment_answer)
+    return cov == 0 if comment_answer == DeepCover::Tools::NOT_EXECUTED
+    return true if line.strip.start_with?("#")
+
+    if comment_answer.is_a?(String)
+      comment_answer = DeepCover::Tools.strip_when_unimportant(line, comment_answer)
+      line = DeepCover::Tools.strip_when_unimportant(line, line)
+      comment_answer = comment_answer + " " * [line.size - comment_answer.size, 0].max
+      comment_answer.chars.zip(line.chars).each do |a, l|
+        return true if a == ' ' && l =~ /\S/
+      end
+
+      return cov == 0 if comment_answer.include?('x')
+      return cov.nil? if comment_answer.include?('-')
+    else
+      line = DeepCover::Tools.strip_when_unimportant(line, line)
+    end
+
+    if line =~ /\S/
+      cov && cov > 0
+    else
+      cov.nil?
+    end
+  end
+end
+
+RSpec.describe 'line coverage' do
+  each_code_examples('./spec/branch_cover/*.rb', max_files: 0) do |fn, lines, lineno|
+    should have_correct_line_coverage(fn, lines, lineno)
+  end
+end
