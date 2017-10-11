4e7d84e3cb3b4c7925776c555730f21fb6e2dc03
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-10-10 20:15:52 -0400

    Merge If and Unless nodes

--- lib/deep_cover/node/if.rb
+++ lib/deep_cover/node/if.rb
@@ -28,49 +28,20 @@ module DeepCover
       include Branch
       has_tracker :truthy
       has_child condition: Node, rewrite: '((%{node}) && %{truthy_tracker})'
-      has_child true_branch: [Node, nil], flow_entry_count: :truthy_tracker_hits
-      has_child else_branch: [Node, nil], flow_entry_count: -> { condition.flow_completion_count - truthy_tracker_hits },
-                remap: Else
+      has_child true_branch: [Node, nil], flow_entry_count: :truthy_tracker_hits, remap: :remap_branch
+      has_child false_branch: [Node, nil], flow_entry_count: -> { condition.flow_completion_count - truthy_tracker_hits },
+                remap: :remap_branch
       executed_loc_keys :keyword, :question
 
-      def self.new(base_node, **kwargs)
-        locs = base_node.location.to_hash
-        if locs[:keyword] && locs[:keyword].source == 'unless'
-          Unless.new(base_node, **kwargs)
-        else
-          super
-        end
+      def remap_branch(child, child_name)
+        is_unless = loc_hash[:keyword] && loc_hash[:keyword].source == 'unless'
+        Else if child_name == :true_branch && is_unless || child_name == :false_branch && !is_unless
       end
 
       def branches
         [
-          true_branch || TrivialBranch.new(condition, else_branch),
-          else_branch
-        ]
-      end
-
-      def execution_count
-        condition.flow_completion_count
-      end
-    end
-
-    class Unless < Node
-      include Branch
-      has_tracker :truthy
-      has_child condition: Node, rewrite: '((%{node}) && %{truthy_tracker})'
-      has_child false_branch: [Node, nil], flow_entry_count: -> { condition.flow_completion_count - truthy_tracker_hits }
-      has_child else_branch: [Node, nil], flow_entry_count: :truthy_tracker_hits,
-                remap: Else
-      executed_loc_keys :keyword
-
-      def initialize(base_node, base_children: base_node.children, **kwargs)
-        super(base_node, base_children: base_children.values_at(0, 2, 1), **kwargs)
-      end
-
-      def branches
-        [
-            false_branch || TrivialBranch.new(condition, else_branch),
-            else_branch
+          true_branch || TrivialBranch.new(condition, false_branch),
+          false_branch || TrivialBranch.new(condition, true_branch)
         ]
       end
 
