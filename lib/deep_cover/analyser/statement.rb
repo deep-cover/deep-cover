2084c2a120f39459da3f14569ab1f8277e3ea899
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-18 17:28:23 -0400

    Refactor with Statement struct

--- lib/deep_cover/analyser/statement.rb
+++ lib/deep_cover/analyser/statement.rb
@@ -1,23 +1,4 @@
 module DeepCover
-  Statement = Struct.new(:node, :sub_statements) do
-    def range
-      node.loc_hash[:expression]
-    end
-
-    # returns a list of [proper_range, node], where
-    # the nodes may be repeating, the proper_ranges are non-intersecting but non ordered.
-    def shatter
-      subs = sub_statements.map(&:range).compact
-      subs.reject!{|r| r.disjoint?(range) } # This is the case iff using heredocs
-      proper = range.split(*subs)
-      proper.map!{|r| r.lstrip(/(\s*#.*\n)+/) }  # Strip comment blocks
-      proper.map!{|r| r.strip(/[^a-zA-Z0-9'"\[\]{}_:$]*/) } # Ignore whitespace & punctuation
-      proper.reject!(&:empty?)
-      proper.reject!{|r| r.source == 'end' || r.source == '}'}
-      proper.map { |r| [r, node] } + sub_statements.flat_map(&:shatter)
-    end
-  end
-
   class Analyser::Statement
     attr_reader :node_runs
 
@@ -28,20 +9,34 @@ module DeepCover
 
     # Returns a map of Range => runs
     def results
-      node_to_statements(@ast)
-        .flat_map(&:shatter)
-        .map{|r, node| [r, @node_runs[node]] }
-        .to_h
+      each_statement(@ast).flat_map do |node, sub_statements|
+        shatter(node, sub_statements).map{|r| [r, @node_runs[node]]}
+      end.to_h
     end
 
     private
+
+    # returns a list of [proper_range, node], where
+    # the nodes may be repeating, the proper_ranges are non-intersecting but non ordered.
+    def shatter(node, sub_statements)
+      subs = sub_statements.map{|n| n.loc_hash[:expression]}.compact
+      range = node.loc_hash[:expression]
+      subs.reject!{|r| r.disjoint?(range) } # This is the case iff using heredocs
+      proper = range.split(*subs)
+      proper.map!{|r| r.lstrip(/(\s*#.*\n)+/) }  # Strip comment blocks
+      proper.map!{|r| r.strip(/[^a-zA-Z0-9'"\[\]{}_:$]*/) } # Ignore whitespace & punctuation
+      proper.reject!(&:empty?)
+      proper.reject!{|r| r.source == 'end' || r.source == '}'}
+      proper
+    end
 
     # Produce a simplified AST such that the nodes are either statements,
     # or sub-statements with execution incompatible with their enclosing {sub-}statement.
     STATEMENTS = Set[Node::Def, Node::Defs, Node::Module, Node::Class, Node::Sclass]
     STATEMENT_GROUPINGS = Set[Node::Begin, Node::Kwbegin, Node::Root]
-    # Returns an array of Statements
-    def node_to_statements(node, parent_statement_node = node.parent)
+    # Returns an array of children
+    def each_statement(node, parent_statement_node = node.parent, &block)
+      return to_enum(:each_statement, node, parent_statement_node) unless block_given?
       keep = if node.loc_hash[:expression].nil?
         false
       elsif node.is_statement
@@ -50,11 +45,18 @@ module DeepCover
         !compatible_runs?(@node_runs[parent_statement_node], @node_runs[node])
       end
 
-      subs = ->(parent) { node.children_nodes.flat_map{|c| node_to_statements(c, parent)} }
+      process_subs = ->(parent) do
+        node.children_nodes.flat_map do |c|
+          each_statement(c, parent, &block)
+        end
+      end
+
       if keep
-        [Statement.new(node, subs[node])]
+        s = process_subs[node]
+        yield node, s
+        [node]
       else
-        subs[parent_statement_node]
+        process_subs[parent_statement_node]
       end
     end
 
