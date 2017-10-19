aa1a35070c3780dcfa1362f7358f34ea435a1a27
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-10-19 18:38:37 -0400

    Make Send's method_name a wrapped node

--- lib/deep_cover/node/send.rb
+++ lib/deep_cover/node/send.rb
@@ -2,12 +2,33 @@ require_relative 'literals'
 
 module DeepCover
   class Node
+    class MethodName < Node
+      include Wrapper
+      has_child name: Symbol
+
+      def initialize(name, parent: raise, **kwargs)
+        super(parent, parent: parent, base_children: [name], **kwargs)
+      end
+
+      def loc_hash
+        {expression: parent.loc_hash[:expression]}
+      end
+
+      def executable?
+        false
+      end
+    end
+
     class Send < Node
       check_completion
       has_child receiver: [Node, nil]
-      has_child method_name: Symbol
-      has_extra_children arguments: Node
+      has_child method_name_wrapper: {Symbol => MethodName}
+      has_extra_children arguments: Node, rewrite: :add_parentheses
       executed_loc_keys :dot, :selector_begin, :selector_end, :operator
+
+      def method_name
+        method_name_wrapper.name
+      end
 
       def loc_hash
         base = super
--- spec/analyser/node_spec.rb
+++ spec/analyser/node_spec.rb
@@ -12,7 +12,7 @@ module DeepCover
       Analyser::Node.new(node, allow_uncovered: allow_uncovered)
     }
     let(:results) { analyser.results }
-    let(:not_executed) { results.select{|_node, runs| runs == 0}.keys }
+    let(:not_executed) { results.select{|node, runs| node.executable? && runs == 0}.keys }
     subject { not_executed.map(&:type) }
 
     context 'when allowing uncovered default arguments' do
