76592cfc27df9d8373097de503e6e57f9a01e73b
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-20 19:53:05 -0400

    Tweak PerLine to use Analyser's mechanisms

--- lib/deep_cover/analyser/per_line.rb
+++ lib/deep_cover/analyser/per_line.rb
@@ -2,16 +2,17 @@ module DeepCover
   class Analyser::PerLine < Analyser
     # Returns an array of runs, one per line.
     def results
+      disallow_partial = !options.fetch(:allow_partial, true)
       line_hits = Array.new(covered_code.nb_lines)
-      covered_code.each_node do |node|
-        if node.executable?
+      each_node do |node, _children|
+        if (runs = node_runs(node))
           node.executed_locs.each do |loc|
             lineno = loc.line - 1
-            if @options[:allow_partial] == false
-              line_hits[lineno] = 0 if node.execution_count == 0
+            if disallow_partial
+              line_hits[lineno] = 0 if runs == 0
               next if line_hits[lineno] == 0
             end
-            line_hits[lineno] = [line_hits[lineno] || 0, node.execution_count].max
+            line_hits[lineno] = [line_hits[lineno] || 0, runs].max
           end
         end
       end
