c66f8bd5e33d63b650750d46997252e4d19fc215
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-21 23:32:37 -0400

    + Node::EmptyBody

---
+++ lib/deep_cover/node/empty_body.rb
@@ -0,0 +1,18 @@
+module DeepCover
+  class Node::EmptyBody < Node
+    def initialize(base_node, parent: raise, index: 0, position: ChildCanBeEmpty.last_empty_position)
+      @position = position
+      @position = parent.expression.begin if @position == true # Some random position... don't rewrite!
+      super(base_node, parent: parent, index: index, base_children: [])
+    end
+
+    def type
+      :EmptyBody
+    end
+
+    def loc_hash
+      {expression: @position}
+    end
+
+  end
+end
