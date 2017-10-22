22441c469ee835bbb783dd9230a12a8cca9b0fff
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-21 21:26:13 -0400

    + TrivialBranch#execution_count, flow_entry_count, executable?

--- lib/deep_cover/node/branch.rb
+++ lib/deep_cover/node/branch.rb
@@ -14,8 +14,13 @@ module DeepCover
     end
 
     class TrivialBranch < Struct.new(:condition, :other_branch)
-      def flow_completion_count
+      def flow_entry_count
         condition.flow_completion_count - other_branch.flow_entry_count
+      end
+      alias_method :flow_completion_count, :flow_entry_count
+      alias_method :execution_count, :flow_entry_count
+      def executable?
+        true
       end
     end
 
