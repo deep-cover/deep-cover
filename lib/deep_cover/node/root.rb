d55c3d3daeb4b55bfb9a9b39df595e57344b83f6
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-21 22:29:06 -0400

    Root might has well have a correct flow_entry_count

--- lib/deep_cover/node/root.rb
+++ lib/deep_cover/node/root.rb
@@ -2,12 +2,12 @@ module DeepCover
   class Node::Root < Node
     has_tracker :root
     has_child main: [Node, nil],
-              flow_entry_count: :root_tracker_hits,
               is_statement: true,
               rewrite: -> {
                 "#{covered_code.trackers_setup_source};%{root_tracker};%{node}"
               }
     attr_reader :covered_code
+    alias_method :flow_entry_count, :root_tracker_hits
 
     def initialize(child_ast, covered_code)
       @covered_code = covered_code
