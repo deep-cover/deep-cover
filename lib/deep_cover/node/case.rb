0d06538c277d795e3f9c5de51068cf03e4143908
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-22 23:12:39 -0400

    - CaseElse. Use can_be_empty and executed_loc_keys

--- lib/deep_cover/node/case.rb
+++ lib/deep_cover/node/case.rb
@@ -42,7 +42,6 @@ module DeepCover
     end
 
     class When < Node
-      # include Branch
       has_tracker :body_entry
       has_extra_children matches: { splat: WhenSplatCondition, Parser::AST::Node => WhenCondition }
       has_child body: Node,
@@ -64,35 +63,31 @@ module DeepCover
       end
     end
 
-    class CaseElse < Node
-      include Wrapper
-      has_tracker :entry
-      has_child body: [Node, nil], rewrite: "((%{entry_tracker};%{node}))",
-                is_statement: true,
-                flow_entry_count: :entry_tracker_hits
-      executed_loc_keys :else
-
-      def flow_entry_count
-        return entry_tracker_hits if body
-        parent.flow_completion_count - parent.children[1...index].map(&:body).map(&:flow_completion_count).inject(0, :+)
-      end
-
-      def loc_hash
-        {else: parent.loc_hash[:else]}
-      end
-    end
-
     class Case < Node
-      # include Branch
-      check_completion
+      include Branch
+      has_tracker :else_entry
       has_child evaluate: [Node, nil]
       has_extra_children whens: When
-      has_child else: { NilClass => CaseElse, Parser::AST::Node => CaseElse }
+      has_child else: Node,
+                can_be_empty: -> { base_node.loc.end.begin },
+                rewrite: -> { "#{'else;' unless has_else?}((%{else_entry_tracker};nil;%{node}))" },
+                executed_loc_keys: [:else],
+                is_statement: true,
+                flow_entry_count: :else_entry_tracker_hits
+
       executed_loc_keys :begin, :end, :keyword
+
+      def branches
+        whens.map(&:body) << self.else
+      end
 
       def execution_count
         return evaluate.flow_completion_count if evaluate
         flow_entry_count
+      end
+
+      def has_else?
+        !!base_node.loc.to_hash[:else]
       end
     end
   end
