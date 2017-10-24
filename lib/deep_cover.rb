43c9b7d7be784a4e726b694384534c9dea5a1671
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-24 00:01:43 -0400

    Move code to Base

--- lib/deep_cover.rb
+++ lib/deep_cover.rb
@@ -5,38 +5,6 @@ module DeepCover
   Misc.require_relative_dir 'deep_cover/parser_ext'
   Misc.require_relative_dir 'deep_cover', except: %w[auto_run builtin_takeover]
 
-  class << self
-    def start
-      return if @started
-      if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
-        # No issues with autoload in jruby, so no need to override it!
-      else
-        require_relative 'deep_cover/core_ext/autoload_overrides'
-        autoload_tracker.initialize_autoloaded_paths
-      end
-      require_relative 'deep_cover/core_ext/require_overrides'
-      @started = true
-    end
-
-    def line_coverage(filename)
-      cover.line_coverage(filename)
-    end
-
-    def covered_code(filename)
-      cover.covered_code(filename)
-    end
-
-    def cover
-      @cover ||= Coverage.new
-    end
-
-    def custom_requirer
-      @custom_requirer ||= CustomRequirer.new
-    end
-
-    def autoload_tracker
-      @autoload_tracker ||= AutoloadTracker.new
-    end
-  end
+  extend Base
 end
 DeepCover::GLOBAL_BINDING = binding
---
+++ lib/deep_cover/base.rb
@@ -0,0 +1,35 @@
+module DeepCover
+  module Base
+    def start
+      return if @started
+      if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
+        # No issues with autoload in jruby, so no need to override it!
+      else
+        require_relative 'core_ext/autoload_overrides'
+        autoload_tracker.initialize_autoloaded_paths
+      end
+      require_relative 'core_ext/require_overrides'
+      @started = true
+    end
+
+    def line_coverage(filename)
+      cover.line_coverage(filename)
+    end
+
+    def covered_code(filename)
+      cover.covered_code(filename)
+    end
+
+    def cover
+      @cover ||= Coverage.new
+    end
+
+    def custom_requirer
+      @custom_requirer ||= CustomRequirer.new
+    end
+
+    def autoload_tracker
+      @autoload_tracker ||= AutoloadTracker.new
+    end
+  end
+end
