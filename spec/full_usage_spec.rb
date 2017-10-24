31cdfcf66c07ac45e1e1427a7d3516de4e90c35e
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-24 00:47:17 -0400

    Create matcher for full usage

--- spec/full_usage_spec.rb
+++ spec/full_usage_spec.rb
@@ -1,22 +1,37 @@
 require "spec_helper"
 
-RSpec.describe 'DeepCover usage' do
-  it 'run `ruby spec/full_usage/simple/simple.rb` successfully' do
+RSpec::Matchers.define :run_successfully do
+  match do |path|
     reader, writer = IO.pipe
-    options = {4 => writer.fileno, in: File::NULL, out: File::NULL, err: File::NULL}
+    error_r, error_w = IO.pipe
+    options = {4 => writer.fileno, in: File::NULL, out: File::NULL, err: error_w}
 
-    pid = spawn('ruby', 'spec/full_usage/simple/simple.rb', '4', options)
+    pid = spawn('ruby', "spec/full_usage/#{path}", '4', options)
     writer.close
-    problems = reader.read
+    error_w.close
+    @output = reader.read.chomp
+    @errors = error_r.read.chomp
     Process.wait(pid)
-    exit_code = $?.exitstatus
+    @exit_code = $?.exitstatus
     reader.close
+    @ouput_ok = @expected_output.nil? || @expected_output == @output
 
-    if problems.empty?
-      fail "Test program should have returned something but didn't"
-    elsif problems != "Done\n"
-      fail "Received unexpected message from test program:\n#{problems}"
-    end
-    exit_code.should be 0
+    @exit_code == 0 && @ouput_ok && @errors == ''
   end
+
+  chain :and_output do |output|
+    @expected_output = output
+  end
+
+  failure_message do
+    [
+      ("expected output '#{@expected_output}', got '#{@output}'" unless @ouput_ok),
+      ("expected exit code 0, got #{@exit_code}" if @exit_code != 0),
+      ("expected no errors, got '#{@errors}'" unless @errors.empty?),
+    ].compact.join(' and ')
+  end
+end
+
+RSpec.describe 'DeepCover usage' do
+  it { 'simple/simple.rb'.should run_successfully.and_output('Done') }
 end
