2e47287c08082b9ed185590a60490470c94a9fd6
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-24 20:57:33 -0400

    Move Tools.format_char_cover

---
+++ lib/deep_cover/tools/format_char_cover.rb
@@ -0,0 +1,18 @@
+module DeepCover
+  module Tools::FormatCharCover
+    COLOR = {'x' => :red, ' ' => :green, '-' => :faint}
+    WHITESPACE_MAP = Hash.new{|_, v| v}.merge!(' ' => '·', "\t" => '→ ')
+    def format_char_cover(covered_code, show_whitespace: false)
+      bc = covered_code.char_cover
+
+      covered_code.buffer.source_lines.map.with_index do |line, line_index|
+        next line if line.strip =~ /^#[ >]/
+        line.chars.map.with_index do |c, c_index|
+          color = COLOR[bc[line_index][c_index]]
+          c = WHITESPACE_MAP[c] if show_whitespace
+          Term::ANSIColor.send(color, c)
+        end.join
+      end
+    end
+  end
+end
--- spec/tools.rb
+++ spec/tools.rb
@@ -127,21 +127,6 @@ module DeepCover
       generated_lines.join("\n")
     end
 
-    COLOR = {'x' => :red, ' ' => :green, '-' => :faint}
-    WHITESPACE_MAP = Hash.new{|_, v| v}.merge!(' ' => '·', "\t" => '→ ')
-    def format_char_cover(covered_code, show_whitespace: false)
-      bc = covered_code.char_cover
-
-      covered_code.buffer.source_lines.map.with_index do |line, line_index|
-        next line if line.strip =~ /^#[ >]/
-        line.chars.map.with_index do |c, c_index|
-          color = COLOR[bc[line_index][c_index]]
-          c = WHITESPACE_MAP[c] if show_whitespace
-          Term::ANSIColor.send(color, c)
-        end.join
-      end
-    end
-
     def parse_cov_comments_answers(lines)
       answers = []
       line_index = 0
