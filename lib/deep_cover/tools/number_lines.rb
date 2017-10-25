9cd972ac7c9b536db37c0cce89b0d5ac64729624
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-24 21:01:24 -0400

    Move Tools.number_lines

---
+++ lib/deep_cover/tools/number_lines.rb
@@ -0,0 +1,18 @@
+module DeepCover
+  module Tools::NumberLines
+    def number_lines(lines, lineno: 1, bad_linenos: [])
+      max_lineno = lineno + lines.size - 1
+      nb_lineno_digits = max_lineno.to_s.size
+      lines.map.with_index do |line, i|
+        cur_lineno = lineno + i
+        cur_lineno_s = cur_lineno.to_s.rjust(nb_lineno_digits)
+        if bad_linenos.include?(cur_lineno)
+          prefix = Term::ANSIColor.red("#{cur_lineno_s} | ")
+        else
+          prefix = Term::ANSIColor.white("#{cur_lineno_s} | ")
+        end
+        "#{prefix}#{line}"
+      end
+    end
+  end
+end
--- spec/tools.rb
+++ spec/tools.rb
@@ -62,21 +62,6 @@ module DeepCover
     extend self
     attr_accessor :current_ast
 
-    def number_lines(lines, lineno: 1, bad_linenos: [])
-      max_lineno = lineno + lines.size - 1
-      nb_lineno_digits = max_lineno.to_s.size
-      lines.map.with_index do |line, i|
-        cur_lineno = lineno + i
-        cur_lineno_s = cur_lineno.to_s.rjust(nb_lineno_digits)
-        if bad_linenos.include?(cur_lineno)
-          prefix = Term::ANSIColor.red("#{cur_lineno_s} | ")
-        else
-          prefix = Term::ANSIColor.white("#{cur_lineno_s} | ")
-        end
-        "#{prefix}#{line}"
-      end
-    end
-
     def builtin_coverage(source, fn, lineno)
       fn = File.absolute_path(File.expand_path(fn))
       ::Coverage.start
