37a5a1acfb3fa8cbc9df2df16fdb8dcee875d971
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-29 21:00:26 -0400

    Donc make up positions

--- lib/deep_cover/node/empty_body.rb
+++ lib/deep_cover/node/empty_body.rb
@@ -2,7 +2,6 @@ module DeepCover
   class Node::EmptyBody < Node
     def initialize(base_node, parent: raise, index: 0, position: ChildCanBeEmpty.last_empty_position)
       @position = position
-      @position = parent.expression.begin if @position == true # Some random position... don't rewrite!
       super(base_node, parent: parent, index: index, base_children: [])
     end
 
@@ -11,6 +10,7 @@ module DeepCover
     end
 
     def loc_hash
+      return {} if @position == true
       {expression: @position}
     end
 
