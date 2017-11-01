bec0a483a44875a30b14e29ce9989f5efea8e063
Marc-Andre Lafortune <github@marc-andre.ca>
2017-11-01 10:56:12 -0400

    Backport Pathname#binwrite too

--- lib/deep_cover/backports.rb
+++ lib/deep_cover/backports.rb
@@ -7,6 +7,9 @@ class Pathname
   def write(*args)
     File.write(to_path, *args)
   end unless method_defined? :write
+  def binwrite(*args)
+    File.binwrite(to_path, *args)
+  end unless method_defined? :binwrite
 end
 require 'backports/2.1.0/module/include'
 require 'backports/2.1.0/enumerable/to_h'
