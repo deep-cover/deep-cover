e2d3975cb6a2e799296defd4afe3e533c721a176
Marc-Andre Lafortune <github@marc-andre.ca>
2017-10-31 20:42:28 -0400

    Refactor cli test in a desperate attempt of fixing Travis

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,27 +2,36 @@ require "spec_helper"
 
 module DeepCover
   RSpec.describe 'CLI' do
-    describe 'deep-cover' do
-      it 'can cover a simple gem' do
-        out = `exe/deep-cover spec/cli_fixtures/trivial_gem -o=false --no-bundle`
-        out.should =~ Regexp.new(%w[trivial_gem.rb 83.33 100 50].join('[ |]*'))
-        out.should include '2 examples, 0 failures'
+    describe 'The output of deep-cover' do
+      let(:command) { "exe/deep-cover spec/cli_fixtures/#{path} -o=false --no-bundle"}
+      let(:output) { Bundler.with_clean_env{ `#{command}` } }
+      subject { output }
+      describe 'for a simple gem' do
+        let(:path) { 'trivial_gem' }
+        it do
+          should =~ Regexp.new(%w[trivial_gem.rb 83.33 100 50].join('[ |]*'))
+          should include '2 examples, 0 failures'
+        end
       end
 
-      it 'can cover a single component gem like activesupport' do
-        out = `exe/deep-cover spec/cli_fixtures/rails_like_gem/component_gem -o=false --no-bundle`
-        out.should =~ Regexp.new(%w[component_gem.rb 80 100 50].join('[ |]*'))
-        out.should include '1 example, 0 failures'
-        out.should_not include 'another_component'
+      describe 'for a single component gem like activesupport' do
+        let(:path) { 'rails_like_gem/component_gem' }
+        it do
+          should =~ Regexp.new(%w[component_gem.rb 80 100 50].join('[ |]*'))
+          should include '1 example, 0 failures'
+          should_not include 'another_component'
+        end
       end
 
-      it 'can cover a multiple component gem like rails' do
-        out = `exe/deep-cover spec/cli_fixtures/rails_like_gem -o=false --no-bundle`
-        out.should =~ Regexp.new(%w[component_gem.rb 80 100 50].join('[ |]*'))
-        out.should =~ Regexp.new(%w[foo.rb 100 100 100].join('[ |]*'))
-        out.should include '1 example, 0 failures'
-        out.should include 'another_component'
-        out.should include '2 examples, 1 failure'
+      describe 'for a multiple component gem like rails' do
+        let(:path) { 'rails_like_gem' }
+        it do
+          should =~ Regexp.new(%w[component_gem.rb 80 100 50].join('[ |]*'))
+          should =~ Regexp.new(%w[foo.rb 100 100 100].join('[ |]*'))
+          should include '1 example, 0 failures'
+          should include 'another_component'
+          should include '2 examples, 1 failure'
+        end
       end
     end
   end
