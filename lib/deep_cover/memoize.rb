d3cb33b6e5d11b4d3976249bcba9be5115d71960
Marc-Andre Lafortune <github@marc-andre.ca>
2017-11-02 15:09:56 -0400

    + memoize

---
+++ lib/deep_cover/memoize.rb
@@ -0,0 +1,46 @@
+module DeepCover
+  # Memoize is a quick way to prepend a module that defines
+  # the memoized methods as `@_cache ||= super.freeze`
+  # It also refines `freeze` to precache memoized methods
+  #
+  module Memoize
+    def self.included(base)
+      base.extend ClassMethods
+    end
+
+    def freeze
+      self.class.memoized.each do |method|
+        send method
+      end
+      super
+    end
+
+    module ClassMethods
+      def memoized
+        @memoized ||= [].freeze
+      end
+
+      def memoizer_module
+        @memoizer_module ||= begin
+          mod = const_set(:Memoizer, Module.new)
+          prepend mod
+          mod
+        end
+      end
+
+      def memoize(*methods)
+        @memoized ||= []
+        @memoized |= methods
+        @memoized.freeze
+
+        methods.each do |method|
+          memoizer_module.module_eval <<-RUBY, __FILE__, __LINE__ + 1
+            def #{method}                    # def foo
+              @_#{method} ||= super.freeze   #   @_foo ||= super.freeze
+            end                              # end
+          RUBY
+        end
+      end
+    end
+  end
+end
---
+++ spec/memoize_spec.rb
@@ -0,0 +1,75 @@
+require "spec_helper"
+
+module DeepCover
+  RSpec.describe Memoize do
+    class Test
+      include Memoize
+      memoize :foo
+      def initialize
+        @val = 0
+      end
+
+      def foo
+        @val += 1
+      end
+    end
+
+    class TestFrozen < Test
+      memoize :foo
+      def initialize
+        super
+        freeze
+      end
+    end
+
+    class TestInheritance < Test
+      memoize :foo, :qux
+      def foo
+        100 + super
+      end
+
+      def baz
+        @baz ||= 0
+        @baz += 1
+      end
+
+      def qux
+        [42]
+      end
+    end
+
+    let(:o) { klass.new }
+    let(:subject) { o }
+    describe "Hot class" do
+      let(:klass) { Test }
+      it { klass.memoized.should =~ [:foo] }
+      it 'maintains arity 0' do
+        klass.instance_method(:foo).arity.should == 0
+      end
+      it 'is memoized' do
+        o.foo.should == 1
+        o.foo.should == 1
+      end
+    end
+
+    describe "Inheritance" do
+      let(:klass) { TestInheritance }
+      it { klass.memoized.should =~ [:foo, :qux] }
+      it 'is memoized' do
+        o.foo.should == 101
+      end
+      it 'freezes memoized results' do
+        o.qux.should be_frozen
+      end
+    end
+
+    describe "Frozen class" do
+      let(:klass) { TestFrozen }
+      it { klass.memoized.should =~ [:foo] }
+      it 'is memoized' do
+        o.foo.should == 1
+        o.foo.should == 1
+      end
+    end
+  end
+end
