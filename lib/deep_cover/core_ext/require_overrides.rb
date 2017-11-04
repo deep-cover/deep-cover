1c0e948b6c48c9a4e138b218bfcf6bac0f4ccf96
Marc-Andre Lafortune <github@marc-andre.ca>
2017-11-03 21:42:40 -0400

    Extract ModuleOverride.
    I think Max is going to freak out.

--- lib/deep_cover/core_ext/require_overrides.rb
+++ lib/deep_cover/core_ext/require_overrides.rb
@@ -23,28 +23,7 @@ module DeepCover
       require(File.absolute_path(path, base))
     end
 
-    class << self
-      def active=(active)
-        each do |mod, method_name|
-          mod.send :alias_method, method_name, :"#{method_name}_#{active ? 'with' : 'without'}_deep_cover"
-        end
-      end
-
-      def setup
-        each do |mod, method_name|
-          mod.send :alias_method, :"#{method_name}_without_deep_cover", method_name
-          mod.send :define_method, :"#{method_name}_with_deep_cover", RequireOverride.instance_method(method_name)
-        end
-      end
-
-      def each(&block)
-        [::Kernel, ::Kernel.singleton_class].each do |mod|
-          %i[require require_relative].each do |method_name|
-            yield mod, method_name
-          end
-        end
-      end
-    end
-    setup
+    extend ModuleOverride
+    override ::Kernel, ::Kernel.singleton_class
   end
 end
---
+++ lib/deep_cover/module_override.rb
@@ -0,0 +1,30 @@
+module DeepCover
+  # Helps redefine methods in overriden_modules.
+  # For each methods in Mod, this defines `<method>_with{out}_deep_cover`.
+  # Set `active` to true or false to alias <method> to one or the other.
+  module ModuleOverride
+    attr_reader :overriden_modules
+
+    def active=(active)
+      each do |mod, method_name|
+        mod.send :alias_method, method_name, :"#{method_name}_#{active ? 'with' : 'without'}_deep_cover"
+      end
+    end
+
+    def override(*modules)
+      @overriden_modules = modules
+      each do |mod, method_name|
+        mod.send :alias_method, :"#{method_name}_without_deep_cover", method_name
+        mod.send :define_method, :"#{method_name}_with_deep_cover", instance_method(method_name)
+      end
+    end
+
+    def each(&block)
+      overriden_modules.each do |mod|
+        instance_methods(false).each do |method_name|
+          yield mod, method_name
+        end
+      end
+    end
+  end
+end
