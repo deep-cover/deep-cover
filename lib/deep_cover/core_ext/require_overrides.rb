083d5d1455359794e2b7992f5dafa29ea804e094
Marc-Andre Lafortune <github@marc-andre.ca>
2017-11-03 20:37:34 -0400

    Refactor

--- lib/deep_cover/core_ext/require_overrides.rb
+++ lib/deep_cover/core_ext/require_overrides.rb
@@ -4,42 +4,24 @@
 # each can have been already overwritten individually. (Rubygems only
 # overrides Kernel#require)
 
-class << Kernel
-  alias_method :require_without_coverage, :require
-  def require(path)
-    result = DeepCover.custom_requirer.require(path)
-    if [:not_found, :cover_failed, :not_supported].include?(result)
-      require_without_coverage(path)
-    else
-      result
+[Kernel, Kernel.singleton_class].each do |mod|
+  mod.module_eval do
+    alias_method :require_without_coverage, :require
+    def require(path)
+      result = DeepCover.custom_requirer.require(path)
+      if [:not_found, :cover_failed, :not_supported].include?(result)
+        require_without_coverage(path)
+      else
+        result
+      end
     end
-  end
 
-  def require_relative(path)
-    base = caller(1..1).first[/[^:]+/]
-    raise LoadError, "cannot infer basepath" unless base
-    base = File.dirname(base)
+    def require_relative(path)
+      base = caller(1..1).first[/[^:]+/]
+      raise LoadError, "cannot infer basepath" unless base
+      base = File.dirname(base)
 
-    require(File.absolute_path(path, base))
-  end
-end
-
-module Kernel
-  alias_method :require_without_coverage, :require
-  def require(path)
-    result = DeepCover.custom_requirer.require(path)
-    if [:not_found, :cover_failed, :not_supported].include?(result)
-      require_without_coverage(path)
-    else
-      result
+      require(File.absolute_path(path, base))
     end
-  end
-
-  def require_relative(path)
-    base = caller(1..1).first[/[^:]+/]
-    raise LoadError, "cannot infer basepath" unless base
-    base = File.dirname(base)
-
-    require(File.absolute_path(path, base))
   end
 end
