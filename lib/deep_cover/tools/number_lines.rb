baa7193de5d32977651f18be11705fe81aeebed7
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-11-05 17:16:18 -0500

    Add a star before the bad_linenos in Tools.number_lines Makes copy-paste keep the information

--- lib/deep_cover/tools/number_lines.rb
+++ lib/deep_cover/tools/number_lines.rb
@@ -7,8 +7,10 @@ module DeepCover
         cur_lineno = lineno + i
         cur_lineno_s = cur_lineno.to_s.rjust(nb_lineno_digits)
         if bad_linenos.include?(cur_lineno)
+          cur_lineno_s = "*#{cur_lineno_s}" unless bad_linenos.empty?
           prefix = Term::ANSIColor.red("#{cur_lineno_s} | ")
         else
+          cur_lineno_s = " #{cur_lineno_s}" unless bad_linenos.empty?
           prefix = Term::ANSIColor.white("#{cur_lineno_s} | ")
         end
         "#{prefix}#{line}"
