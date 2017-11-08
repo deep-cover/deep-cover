1f86f76c65340d791a3ceea828dcd9773af71d1b
Marc-Andre Lafortune <github@marc-andre.ca>
2017-11-08 13:31:48 -0500

    Isolate output for Debugger

--- lib/deep_cover/cli/debugger.rb
+++ lib/deep_cover/cli/debugger.rb
@@ -41,34 +41,36 @@ module DeepCover
       end
 
       def show_line_coverage
-        puts "Line Coverage: Builtin | DeepCover | DeepCover Strict:\n"
+        output { "Line Coverage: Builtin | DeepCover | DeepCover Strict:\n" }
         begin
           builtin_line_coverage = builtin_coverage(@source, @filename, @lineno)
           our_line_coverage = our_coverage(@source, @filename, @lineno, **options)
           our_strict_line_coverage = our_coverage(@source, @filename, @lineno, allow_partial: false, **options)
-          lines = format(builtin_line_coverage, our_line_coverage, our_strict_line_coverage, source: @source)
-          puts number_lines(lines, lineno: @lineno)
+          output do
+            lines = format(builtin_line_coverage, our_line_coverage, our_strict_line_coverage, source: @source)
+            number_lines(lines, lineno: @lineno)
+          end
         rescue Exception => e
-          puts "Can't run coverage: #{e.class.name}: #{e}\n#{e.backtrace.join("\n")}"
+          output { "Can't run coverage: #{e.class.name}: #{e}\n#{e.backtrace.join("\n")}" }
           @failed = true
         end
       end
 
       def show_instrumented_code
-        puts "\nInstrumented code:\n"
-        puts format_generated_code(covered_code)
+        output { "\nInstrumented code:\n" }
+        output { format_generated_code(covered_code) }
       end
 
       def show_ast
-        puts "\nParsed code:\n"
+        output { "\nParsed code:\n" }
         Node.prepend ColorAST
-        puts covered_code.covered_ast
+        output { covered_code.covered_ast }
       end
 
       def show_char_coverage
-        puts "\nChar coverage:\n"
+        output { "\nChar coverage:\n" }
 
-        puts format_char_cover(covered_code, show_whitespace: !!ENV['W'], **options)
+        output { format_char_cover(covered_code, show_whitespace: !!ENV['W'], **options) }
       end
 
       def pry
@@ -88,9 +90,9 @@ module DeepCover
       def execute
         begin
           execute_sample(covered_code)
-          # puts *trace_counts  # Keep for low-level debugging purposes
+          # output { trace_counts }  # Keep for low-level debugging purposes
         rescue Exception => e
-          puts "Can't `execute_sample`:#{e.class.name}: #{e}\n#{e.backtrace.join("\n")}"
+          output { "Can't `execute_sample`:#{e.class.name}: #{e}\n#{e.backtrace.join("\n")}" }
           @failed = true
         end
       end
@@ -105,6 +107,10 @@ module DeepCover
           end
         end.enable { covered_code.freeze }
         all
+      end
+
+      def output
+        puts yield
       end
     end
   end
