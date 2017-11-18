09d1cddc4bf0dd298756b3dca55fc5f6483aef0e
Marc-Andre Lafortune <github@marc-andre.ca>
2017-11-17 20:08:18 -0500

    Split taking over and implementation

--- lib/deep_cover/builtin_takeover.rb
+++ lib/deep_cover/builtin_takeover.rb
@@ -2,3 +2,8 @@
 
 require_relative '../deep_cover'
 require_relative '../deep_cover/core_ext/coverage_replacement'
+
+require 'coverage'
+BuiltinCoverage = Coverage
+Object.send(:remove_const, 'Coverage')
+Coverage = DeepCover::CoverageReplacement.dup
--- lib/deep_cover/core_ext/coverage_replacement.rb
+++ lib/deep_cover/core_ext/coverage_replacement.rb
@@ -2,29 +2,27 @@
 
 # This is a complete replacement for the builtin Coverage module of Ruby
 
-require 'coverage'
-BuiltinCoverage = Coverage
-Object.send(:remove_const, 'Coverage')
-
-module Coverage
-  class << self
-    def start
-      @started = true
-      DeepCover.start
-      DeepCover.coverage.reset
-    end
-
-    def result
-      raise 'coverage measurement is not enabled' unless @started
-      @started = false
-      peek_result
-    end
-
-    def peek_result
-      results = DeepCover.coverage.covered_codes.map do |covered_code|
-        [covered_code.path, covered_code.line_coverage(allow_partial: false)]
+module DeepCover
+  module CoverageReplacement
+    class << self
+      def start
+        @started = true
+        DeepCover.start
+        DeepCover.coverage.reset
       end
-      Hash[results]
+
+      def result
+        raise 'coverage measurement is not enabled' unless @started
+        @started = false
+        peek_result
+      end
+
+      def peek_result
+        results = DeepCover.coverage.covered_codes.map do |covered_code|
+          [covered_code.path, covered_code.line_coverage(allow_partial: false)]
+        end
+        Hash[results]
+      end
     end
   end
 end
