2951e0914f237d267938066e2f013f6b354af099
Marc-Andre Lafortune <github@marc-andre.ca>
2018-02-16 19:04:39 -0500

    Remove duplicated test

--- spec/cli_fixtures/simple_rails42_app/test/models/user_test.rb
+++ spec/cli_fixtures/simple_rails42_app/test/models/user_test.rb
@@ -1,12 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def test_hello
-    u = User.new(name: 'hi')
-
-    assert u.save!
-  end
-
   def test_foo
     u = User.new(name: 'hi')
 
