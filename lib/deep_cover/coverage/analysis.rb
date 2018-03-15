809cd1185aacb5435e49b0a52c83b965b40219ae
Marc-Andre Lafortune <github@marc-andre.ca>
2018-03-15 13:27:05 -0400

    Coverage: repatriate code, yay

--- lib/deep_cover/coverage/analysis.rb
+++ lib/deep_cover/coverage/analysis.rb
@@ -1,46 +1,42 @@
 # frozen_string_literal: true
 
 module DeepCover
-  class Coverage::Analysis < Struct.new(:covered_codes, :options)
-    include Memoize
-    memoize :analyser_map, :stat_map
-
-    def analyser_map
-      covered_codes.map do |covered_code|
-        [covered_code, compute_analysers(covered_code)]
-      end.to_h
-    end
-
-    def stat_map
-      analyser_map.transform_values { |a| a.transform_values(&:stats) }
-    end
-
-    def overall
-      return 100 if stat_map.empty?
-      node, branch = Tools.merge(*stat_map.values, :+).values_at(:node, :branch)
-      (node + branch).percent_covered
-    end
-
-    def self.template
-      {node: Analyser::Node, per_char: Analyser::PerChar, branch: Analyser::Branch}
-    end
-
-    private
-
-    def compute_analysers(covered_code)
-      base = Analyser::Node.new(covered_code, **options)
-      {node: base}.merge!(
-          {
-            per_char: Analyser::PerChar,
-            branch: Analyser::Branch,
-          }.transform_values { |klass| klass.new(base, **options) }
-      )
-    end
-  end
-
   class Coverage
-    def analysis(**options)
-      Analysis.new(covered_codes, options)
+    class Analysis < Struct.new(:covered_codes, :options)
+      include Memoize
+      memoize :analyser_map, :stat_map
+
+      def analyser_map
+        covered_codes.map do |covered_code|
+          [covered_code, compute_analysers(covered_code)]
+        end.to_h
+      end
+
+      def stat_map
+        analyser_map.transform_values { |a| a.transform_values(&:stats) }
+      end
+
+      def overall
+        return 100 if stat_map.empty?
+        node, branch = Tools.merge(*stat_map.values, :+).values_at(:node, :branch)
+        (node + branch).percent_covered
+      end
+
+      def self.template
+        {node: Analyser::Node, per_char: Analyser::PerChar, branch: Analyser::Branch}
+      end
+
+      private
+
+      def compute_analysers(covered_code)
+        base = Analyser::Node.new(covered_code, **options)
+        {node: base}.merge!(
+            {
+              per_char: Analyser::PerChar,
+              branch: Analyser::Branch,
+            }.transform_values { |klass| klass.new(base, **options) }
+        )
+      end
     end
   end
 end
--- lib/deep_cover/coverage/persistence.rb
+++ lib/deep_cover/coverage/persistence.rb
@@ -2,93 +2,95 @@
 
 module DeepCover
   require 'securerandom'
-  class Coverage::Persistence
-    BASENAME = 'coverage.dc'
-    TRACKER_TEMPLATE = 'trackers%{unique}.dct'
+  class Coverage
+    class Persistence
+      BASENAME = 'coverage.dc'
+      TRACKER_TEMPLATE = 'trackers%{unique}.dct'
 
-    attr_reader :dir_path
-    def initialize(dest_path, dirname)
-      @dir_path = Pathname(dest_path).join(dirname).expand_path
-    end
-
-    def load(with_trackers: true)
-      saved?
-      load_trackers if with_trackers
-      load_coverage
-    end
-
-    def save(coverage)
-      create_if_needed
-      delete_trackers
-      save_coverage(coverage)
-    end
-
-    def save_trackers(global)
-      saved?
-      trackers = eval(global) # rubocop:disable Security/Eval
-      # Some testing involves more than one process, some of which don't run any of our covered code.
-      # Don't save anything if that's the case
-      return if trackers.nil?
-      basename = format(TRACKER_TEMPLATE, unique: SecureRandom.urlsafe_base64)
-      dir_path.join(basename).binwrite(Marshal.dump(
-                                           version: DeepCover::VERSION,
-                                           global: global,
-                                           trackers: trackers,
-      ))
-    end
-
-    def saved?
-      raise "Can't find folder '#{dir_path}'" unless dir_path.exist?
-      self
-    end
-
-    private
-
-    def create_if_needed
-      dir_path.mkpath
-    end
-
-    def save_coverage(coverage)
-      dir_path.join(BASENAME).binwrite(Marshal.dump(
-                                           version: DeepCover::VERSION,
-                                           coverage: coverage,
-      ))
-    end
-
-    # rubocop:disable Security/MarshalLoad
-    def load_coverage
-      Marshal.load(dir_path.join(BASENAME).binread).tap do |version:, coverage:|
-        raise "dump version mismatch: #{version}, currently #{DeepCover::VERSION}" unless version == DeepCover::VERSION
-        return coverage
+      attr_reader :dir_path
+      def initialize(dest_path, dirname)
+        @dir_path = Pathname(dest_path).join(dirname).expand_path
       end
-    end
 
-    def load_trackers
-      tracker_files.each do |full_path|
-        Marshal.load(full_path.binread).tap do |version:, global:, trackers:|
+      def load(with_trackers: true)
+        saved?
+        load_trackers if with_trackers
+        load_coverage
+      end
+
+      def save(coverage)
+        create_if_needed
+        delete_trackers
+        save_coverage(coverage)
+      end
+
+      def save_trackers(global)
+        saved?
+        trackers = eval(global) # rubocop:disable Security/Eval
+        # Some testing involves more than one process, some of which don't run any of our covered code.
+        # Don't save anything if that's the case
+        return if trackers.nil?
+        basename = format(TRACKER_TEMPLATE, unique: SecureRandom.urlsafe_base64)
+        dir_path.join(basename).binwrite(Marshal.dump(
+                                             version: DeepCover::VERSION,
+                                             global: global,
+                                             trackers: trackers,
+        ))
+      end
+
+      def saved?
+        raise "Can't find folder '#{dir_path}'" unless dir_path.exist?
+        self
+      end
+
+      private
+
+      def create_if_needed
+        dir_path.mkpath
+      end
+
+      def save_coverage(coverage)
+        dir_path.join(BASENAME).binwrite(Marshal.dump(
+                                             version: DeepCover::VERSION,
+                                             coverage: coverage,
+        ))
+      end
+
+      # rubocop:disable Security/MarshalLoad
+      def load_coverage
+        Marshal.load(dir_path.join(BASENAME).binread).tap do |version:, coverage:|
           raise "dump version mismatch: #{version}, currently #{DeepCover::VERSION}" unless version == DeepCover::VERSION
-          merge_trackers(eval("#{global} ||= {}"), trackers) # rubocop:disable Security/Eval
+          return coverage
         end
       end
-    end
-    # rubocop:enable Security/MarshalLoad
 
-    def merge_trackers(hash, to_merge)
-      hash.merge!(to_merge) do |_key, current, to_add|
-        unless current.empty? || current.size == to_add.size
-          warn "Merging trackers of different sizes: #{current.size} vs #{to_add.size}"
+      def load_trackers
+        tracker_files.each do |full_path|
+          Marshal.load(full_path.binread).tap do |version:, global:, trackers:|
+            raise "dump version mismatch: #{version}, currently #{DeepCover::VERSION}" unless version == DeepCover::VERSION
+            merge_trackers(eval("#{global} ||= {}"), trackers) # rubocop:disable Security/Eval
+          end
         end
-        to_add.zip(current).map { |a, b| a + b }
       end
-    end
+      # rubocop:enable Security/MarshalLoad
 
-    def tracker_files
-      basename = format(TRACKER_TEMPLATE, unique: '*')
-      Pathname.glob(dir_path.join(basename))
-    end
+      def merge_trackers(hash, to_merge)
+        hash.merge!(to_merge) do |_key, current, to_add|
+          unless current.empty? || current.size == to_add.size
+            warn "Merging trackers of different sizes: #{current.size} vs #{to_add.size}"
+          end
+          to_add.zip(current).map { |a, b| a + b }
+        end
+      end
 
-    def delete_trackers
-      tracker_files.each(&:delete)
+      def tracker_files
+        basename = format(TRACKER_TEMPLATE, unique: '*')
+        Pathname.glob(dir_path.join(basename))
+      end
+
+      def delete_trackers
+        tracker_files.each(&:delete)
+      end
     end
   end
 end
