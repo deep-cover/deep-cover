efec0ec230abe9886eab6b5bd4b089843b260056
Maxime Lapointe <hunter_spawn@hotmail.com>
2018-04-30 09:15:47 -0400

    + Parser::Source::Range#wrap_rwhitespace

--- lib/deep_cover/parser_ext/range.rb
+++ lib/deep_cover/parser_ext/range.rb
@@ -4,4 +4,9 @@ class Parser::Source::Range
   def succ
     adjust(begin_pos: +1, end_pos: +1)
   end
+
+  def wrap_rwhitespace
+    whitespace = @source_buffer.slice(end_pos..-1)[/\A\s+/] || ''
+    adjust(end_pos: whitespace.size)
+  end
 end
