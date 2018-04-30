6df4abb199a661a85d736b0e06ce2b11dde482f5
Maxime Lapointe <hunter_spawn@hotmail.com>
2018-04-30 09:17:31 -0400

    Basic emulation of Ruby25's branch coverage

---
+++ lib/deep_cover/analyser/ruby25_like_branch.rb
@@ -0,0 +1,161 @@
+# frozen_string_literal: true
+
+require_relative 'subset'
+
+module DeepCover
+  class Analyser::Ruby25LikeBranch < Analyser
+    def self.human_name
+      'Ruby25 branches'
+    end
+    include Analyser::Subset
+    SUBSET_CLASSES = [Node::Case, Node::Csend, Node::If, Node::ShortCircuit,
+                      Node::Until, Node::UntilPost, Node::While, Node::WhilePost,
+                     ].freeze
+
+    def initialize(*args)
+      super
+      @loc_index = 0
+    end
+
+    def node_runs(node)
+      runs = super
+      if node.is_a?(Node::Branch) && covered?(runs)
+        worst = worst_branch_runs(node)
+        runs = worst unless covered?(worst)
+      end
+      runs
+    end
+
+    def results
+      each_node.map do |node|
+        case node
+        when Node::Case
+          handle_case(node)
+        when Node::Csend
+          handle_csend(node)
+        when Node::If
+          handle_if(node)
+        when Node::ShortCircuit
+          handle_short_circuit(node)
+        when Node::Until, Node::While, Node::UntilPost, Node::WhilePost
+          handle_until_while(node)
+        end
+      end.to_h
+    end
+
+    protected
+
+    def handle_case(node)
+      cond_info = [:case, *node_loc_infos(node)]
+
+      sub_keys = [:when] * (node.branches.size - 1) + [:else]
+      fallbacks = node.whens[1..-1].map { |w| w.loc_hash[:keyword] }
+      fallbacks << (node.loc_hash[:else] || node.loc_hash[:end])
+      fallbacks << node.loc_hash[:end]
+      fallbacks.map!(&:begin)
+
+      clauses_infos = infos_for_branches(node, node.branches, sub_keys, fallbacks)
+
+      [cond_info, clauses_infos]
+    end
+
+    def handle_csend(node)
+      cond_info = [:"&.", *node_loc_infos(node)]
+      false_branch, true_branch = node.branches
+      [cond_info, {[:then, *node_loc_infos(node)] => true_branch.execution_count,
+                   [:else, *node_loc_infos(node)] => false_branch.execution_count,
+                  },
+      ]
+    end
+
+    def handle_if(node)
+      key = node.style == :unless ? :unless : :if
+      cond_info = [key, *node_loc_infos(node)]
+
+      sub_keys = [:then, :else]
+      if node.style == :ternary
+        fallback_locs = [nil, nil]
+      else
+        if node.style == :elsif
+          end_node = node.root_if_node.loc_hash[:end]
+        else
+          end_node = node.loc_hash[:end]
+        end
+        fallback_locs = [node.loc_hash[:else] || end_node, end_node]
+      end
+      # loc can be nil if the clause can't be empty, such as ternary and modifer if/unless
+      fallback_locs.map! { |loc| loc.begin if loc }
+
+      if key == :unless
+        sub_keys.reverse!
+        fallback_locs.reverse!
+      end
+
+      clauses_infos = infos_for_branches(node, node.branches, sub_keys, fallback_locs)
+      [cond_info, clauses_infos]
+    end
+
+    def handle_short_circuit(node)
+      cond_info = [node.operator, *node_loc_infos(node)]
+      branches = node.branches
+      sub_keys = [:then, :else]
+      sub_keys.reverse! if node.is_a?(Node::Or)
+
+      [cond_info, infos_for_branches(node, branches, sub_keys, [nil, nil])]
+    end
+
+    def handle_until_while(node)
+      key = node.is_a?(Node::While) || node.is_a?(Node::WhilePost) ? :while : :until
+      base_info = [key, *node_loc_infos(node)]
+      if node.is_a?(Node::WhilePost) || node.is_a?(Node::UntilPost)
+        if node.body.instructions.present?
+          end_pos = node.body.instructions.last.expression.end_pos
+          body = node.body.instructions.first.expression.with(end_pos: end_pos)
+        else
+          body = node.body.loc_hash[:end].begin
+        end
+      else
+        body = node.body
+      end
+
+      [base_info, {[:body, *node_loc_infos(body)] => node.body.execution_count}]
+    end
+
+    private
+
+    def infos_for_branch(node, branch, key, fallback_loc)
+      if !branch.is_a?(Node::EmptyBody)
+        loc = branch
+      elsif branch.expression && (key != :else || node.loc_hash[:else])
+        # There is a clause, but it is empty
+        # We create a `else` in `case` when there are none, so this special casing is needed for it.
+        # Ruby25 returns the char before the next keyword
+        loc = fallback_loc
+      else
+        loc = node
+      end
+      [[key, *node_loc_infos(loc)], branch.execution_count]
+    end
+
+    def infos_for_branches(node, branches, keys, fallback_locs)
+      branches_infos = branches.map.with_index do |branch, i|
+        infos_for_branch(node, branch, keys[i], fallback_locs[i])
+      end
+      branches_infos.to_h
+    end
+
+    def node_loc_infos(source_range_or_node)
+      if source_range_or_node.is_a?(Node)
+        node = source_range_or_node
+        source_range = node.expression
+        if node.is_a?(Node::If) && node.style == :elsif && (node.true_branch.is_a?(Node::EmptyBody) || node.deepest_elsif_node.loc_hash[:else].nil?)
+          source_range = source_range.wrap_rwhitespace
+        end
+      else
+        source_range = source_range_or_node
+      end
+      @loc_index += 1
+      [@loc_index, source_range.line, source_range.column, source_range.last_line, source_range.last_column]
+    end
+  end
+end
---
+++ spec/analyser/ruby25_like_branch_spec.rb
@@ -0,0 +1,288 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'tempfile'
+require 'coverage'
+require 'active_support/core_ext/string/indent'
+require 'active_support/core_ext/string/strip'
+
+module DeepCover
+  RSpec.describe Analyser::Ruby25LikeBranch do
+    next if RUBY_VERSION < '2.5'
+    Execution = Struct.new(:code, :ruby_result, :dc_result, :raw_dc_result)
+
+    # Mutations are applied automatically to the received code:
+    #   A test will be made with and then without empty lines
+    #   A test will be made with `if` replaced by `unless` (except if there is a elsif)
+    #   A test will be made with `while` replaced by `until !(...)`
+    #   A test will be made with every line containing only a number removed
+    #   A test will be made with every line containing only a number with a second line before (DeepCover)
+    #   Every mix of the above rules will be tested.
+    matcher :have_similar_result_to_ruby do
+      match do |ruby_code|
+        @executions = []
+        ruby_code = ruby_code.strip_heredoc
+        ruby_codes = [ruby_code.rstrip]
+        extra_ruby_codes = ruby_codes.map { |c| c.gsub(/^(\s*)(\d+\s*$)/, '') }
+        extra_ruby_codes.concat(ruby_codes.map { |c| c.gsub(/^(\s*)(\d+\s*)$/, "\\1DeepCover\n\\1\\2") })
+        ruby_codes.concat(extra_ruby_codes)
+        ruby_codes.concat(ruby_codes.map { |c| c.gsub(/\bif\b/, 'unless') }) unless ruby_code[/\belsif\b/]
+        ruby_codes.concat(ruby_codes.map { |c| c.gsub(/\bwhile(.*)\b/, 'until !(\1)') })
+
+        ruby_codes += ruby_codes.map { |c| c.split("\n").select(&:present?).join("\n") }
+        ruby_codes.uniq!
+
+        ruby_codes.each(&method(:execute))
+
+        @different_executions = @executions.reject { |e| e.ruby_result == e.dc_result }
+        @non_uniq_index_executions = @executions.reject { |e| has_unique_index?(e.raw_dc_result) }
+
+        @different_executions.empty? && @non_uniq_index_executions.empty?
+      end
+
+      failure_message do
+        messages = (@different_executions + @non_uniq_index_executions).uniq.map do |execution|
+          msg = []
+          msg << 'For ruby code:'
+          msg << execution.code.rstrip.indent(4)
+
+          if @different_executions.include?(execution)
+            msg << <<-MSG.strip_heredoc
+              Expected something like:
+                  #{execution.ruby_result.sort.to_h}
+              but deep-dover generated something like:
+                  #{execution.dc_result.sort.to_h}
+            MSG
+          end
+
+          if @non_uniq_index_executions.include?(execution)
+            msg << 'The index of the branches (first number) was not uniq, here is the raw output:'
+            msg << "    #{execution.raw_dc_result}"
+          end
+
+          msg.join("\n")
+        end
+
+        messages.join("\n-----------\n")
+      end
+
+      def execute(ruby_code)
+        f = Tempfile.new(['ruby', '.rb'])
+        f.write(ruby_code)
+        f.close
+
+        ::Coverage.start(branches: true)
+        Tools.execute_sample -> { require f.path }
+        ruby_result = ::Coverage.result.values.first[:branches]
+        $LOADED_FEATURES.delete(f.path)
+
+        covered_code = DeepCover::CoveredCode.new(source: ruby_code, path: f.path)
+        Tools.execute_sample(covered_code)
+        raw_dc_result = DeepCover::Analyser::Ruby25LikeBranch.new(covered_code).results
+
+        ruby_result = normalize_result(ruby_result)
+        dc_result = normalize_result(raw_dc_result)
+        @executions << Execution.new(ruby_code, ruby_result, dc_result, raw_dc_result)
+      end
+
+      def normalize_result(result)
+        result.map do |key, sub_hash|
+          key = key.values_at(0, 2..-1)
+          sub_hash = sub_hash.map do |sub_key, nb_hits|
+            sub_key = sub_key.values_at(0, 2..-1)
+            [sub_key, nb_hits]
+          end.to_h
+          [key, sub_hash]
+        end.to_h
+      end
+
+      def has_unique_index?(result)
+        indexes = result.flat_map do |key, sub_hash|
+          [key[1]] + sub_hash.keys.map { |a| a[1] }
+        end
+        indexes == indexes.uniq
+      end
+    end
+
+    # The list of cases to test, separated by ###
+    # Mutations are applied to these test cases, see doc of the #have_similar_result_to_ruby matcher for details
+    cases = <<-RUBY
+      if DeepCover
+        66
+      end
+    ###
+      if DeepCover
+        66
+      elsif DeepCover::Node
+        77
+      end
+    ###
+      if DeepCover
+        66
+      elsif DeepCover::Node
+        77
+      elsif DeepCover::Analyser
+        88
+      end
+    ###
+      if DeepCover
+        66
+      else
+        99
+      end
+    ###
+      if DeepCover
+        66
+      elsif DeepCover::Node
+        77
+      else
+        99
+      end
+    ###
+      if DeepCover
+        66
+      elsif DeepCover::Node
+        77
+      elsif DeepCover::Analyser
+        88
+      else
+        99
+      end
+    ###
+      a = 123 if DeepCover
+    ###
+      DeepCover ? 1 : 0
+    ###
+      case DeepCover
+      when 1
+        11
+      end
+    ###
+      case DeepCover
+      when 1
+        11
+      when 2
+        22
+      end
+    ###
+      case DeepCover
+      when 1
+        11
+      else
+        33
+      end
+    ###
+      case DeepCover
+      when 1
+        11
+      when 2
+        22
+      else
+        33
+      end
+    ###
+      asd = 123
+      asd&.to_i
+    ###
+      asd = nil
+      asd&.to_i
+    ###
+      a = 1
+      while a < 10
+        a += 1
+      end
+    ###
+      a = 1
+      while a < 10
+        DeepCover
+        a += 1
+      end
+    ###
+      a = 10
+      while a < 10
+
+      end
+    ###
+      a = 1
+      a += 1 while a < 10
+    ###
+      a = 1
+      begin
+        a += 1
+      end while a < 10
+    ###
+      a = 1
+      begin
+        DeepCover
+        a += 1
+      end while a < 10
+    ###
+      a = 10
+      begin
+
+      end while a < 10
+    RUBY
+
+    cases.split("###\n").each do |code|
+      it { code.should have_similar_result_to_ruby }
+    end
+
+    def run_dc_ruby25_like_branch(ruby_code)
+      covered_code = DeepCover::CoveredCode.new(source: ruby_code)
+      Tools.execute_sample(covered_code)
+      DeepCover::Analyser::Ruby25LikeBranch.new(covered_code).results
+    end
+
+    it "`123 && 45` gives correct results for &&" do
+      key, branches = run_dc_ruby25_like_branch("123 && 45").first
+      key[2..-1].should == [1, 0, 1, 9]
+
+      then_branch, then_hits = branches.detect{|k, v| k.first == :then }
+      then_hits.should == 1
+      then_branch[2..-1].should == [1, 7, 1, 9]
+
+      else_branch, else_hits = branches.detect{|k, v| k.first == :else }
+      else_hits.should == 0
+      else_branch[2..-1].should == [1, 0, 1, 9]
+    end
+
+    it "`false && 45` gives correct results for &&" do
+      key, branches = run_dc_ruby25_like_branch("false && 45").first
+      key[2..-1].should == [1, 0, 1, 11]
+
+      then_branch, then_hits = branches.detect{|k, v| k.first == :then }
+      then_hits.should == 0
+      then_branch[2..-1].should == [1, 9, 1, 11]
+
+      else_branch, else_hits = branches.detect{|k, v| k.first == :else }
+      else_hits.should == 1
+      else_branch[2..-1].should == [1, 0, 1, 11]
+    end
+
+    it "`123 || 45` gives correct results for ||" do
+      key, branches = run_dc_ruby25_like_branch("123 || 45").first
+      key[2..-1].should == [1, 0, 1, 9]
+
+      then_branch, then_hits = branches.detect{|k, v| k.first == :then }
+      then_hits.should == 1
+      then_branch[2..-1].should == [1, 0, 1, 9]
+
+      else_branch, else_hits = branches.detect{|k, v| k.first == :else }
+      else_hits.should == 0
+      else_branch[2..-1].should == [1, 7, 1, 9]
+    end
+
+    it "`false || 45` gives correct results for ||" do
+      key, branches = run_dc_ruby25_like_branch("false || 45").first
+      key[2..-1].should == [1, 0, 1, 11]
+
+      then_branch, then_hits = branches.detect{|k, v| k.first == :then }
+      then_hits.should == 0
+      then_branch[2..-1].should == [1, 0, 1, 11]
+
+      else_branch, else_hits = branches.detect{|k, v| k.first == :else }
+      else_hits.should == 1
+      else_branch[2..-1].should == [1, 9, 1, 11]
+    end
+  end
+end
