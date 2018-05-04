0fa815dfe20895b432cb42f8d1f752e9b922c1f7
Maxime Lapointe <hunter_spawn@hotmail.com>
2018-05-04 11:49:06 -0400

    Fix indent

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -8,7 +8,7 @@ module DeepCover
     let(:output) do
       require 'open3'
       out, errors, _status = Bundler.with_clean_env do
-       Open3.capture3(command)
+        Open3.capture3(command)
       end
       errors.should match expected_errors unless RUBY_PLATFORM == 'java'
       out
