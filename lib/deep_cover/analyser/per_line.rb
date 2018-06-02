d67d069a8f04bb856a5d0a5079a2516a953bccdb
Maxime Lapointe <hunter_spawn@hotmail.com>
2018-06-02 15:28:16 -0400

    Clearer strictness logic for Analyzer::PerLine

--- lib/deep_cover/analyser/per_line.rb
+++ lib/deep_cover/analyser/per_line.rb
@@ -4,20 +4,21 @@ module DeepCover
   class Analyser::PerLine < Analyser
     # Returns an array of runs, one per line.
     def results
-      disallow_partial = !options.fetch(:allow_partial, true)
+      allow_partial = options.fetch(:allow_partial, true)
       line_hits = Array.new(covered_code.nb_lines + covered_code.lineno - 1)
+      disallowed_lines = Set.new
       each_node do |node|
         next unless (runs = node_runs(node))
         node.executed_locs.each do |loc|
           line_index = loc.line - 1
-          if disallow_partial
-            line_hits[line_index] = 0 if runs == 0
-            next if line_hits[line_index] == 0
-          end
+
+          next if disallowed_lines.include?(line_index)
+          disallowed_lines << line_index if [nil, false, :branch].include?(allow_partial) && runs == 0
+
           line_hits[line_index] = [line_hits[line_index] || 0, runs].max
         end
       end
-
+      disallowed_lines.each { |line_index| line_hits[line_index] = 0 }
       line_hits
     end
   end
