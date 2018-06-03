430eb43556c9406a8f2619406f9cb7a9b9156265
Maxime Lapointe <hunter_spawn@hotmail.com>
2018-06-03 10:22:13 -0400

    #wrap_rwhitespace can now be given a regex for whitespace

--- lib/deep_cover/parser_ext/range.rb
+++ lib/deep_cover/parser_ext/range.rb
@@ -5,16 +5,16 @@ class Parser::Source::Range
     adjust(begin_pos: +1, end_pos: +1)
   end
 
-  def wrap_rwhitespace
-    whitespace = @source_buffer.slice(end_pos..-1)[/\A\s+/] || ''
+  def wrap_rwhitespace(whitespaces: /\A\s+/)
+    whitespace = @source_buffer.slice(end_pos..-1)[whitespaces] || ''
     adjust(end_pos: whitespace.size)
   end
 
-  def wrap_rwhitespace_and_comments
-    current = wrap_rwhitespace
+  def wrap_rwhitespace_and_comments(whitespaces: /\A\s+/)
+    current = wrap_rwhitespace(whitespaces: whitespaces)
     while @source_buffer.slice(current.end_pos) == '#'
       comment = @source_buffer.slice(current.end_pos..-1)[/\A[^\n]+/] || ''
-      current = current.adjust(end_pos: comment.size).wrap_rwhitespace
+      current = current.adjust(end_pos: comment.size).wrap_rwhitespace(whitespaces: whitespaces)
     end
     current
   end
