435087e172087f3cda639c93e4a41f0e3226d3e1
Marc-Andre Lafortune <github@marc-andre.ca>
2018-07-07 00:23:06 -0400

    Don't overly limit paths here, we might want to be further down the path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,7 +32,7 @@ end
 def trivial_gem_coverage
   # We can only easily require this at most once, so we have to share...
   $trivial_gem_coverage ||= begin
-    DeepCover.cover(paths: 'spec') { require_relative 'cli_fixtures/trivial_gem/lib/trivial_gem' }
+    DeepCover.cover(paths: '.') { require_relative 'cli_fixtures/trivial_gem/lib/trivial_gem' }
     require_relative 'cli_fixtures/trivial_gem/lib/trivial_gem'
     # Kind of a pain to load the test properly, so cheat...
     TrivialGem.hello
