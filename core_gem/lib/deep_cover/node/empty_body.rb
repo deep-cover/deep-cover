330182e2ae88d419e217a83dcdd5a5a6a779cd70
Marc-Andre Lafortune <github@marc-andre.ca>
2018-08-29 15:29:30 -0400

    Typo [ci skip]

--- core_gem/lib/deep_cover/node/empty_body.rb
+++ core_gem/lib/deep_cover/node/empty_body.rb
@@ -21,7 +21,7 @@ module DeepCover
     end
 
     # When parent rewrites us, the %{node} must always be at the beginning because our location can
-    # also be rewritten by out parent, and we want the rewrite to be after it.
+    # also be rewritten by our parent, and we want the rewrite to be after it.
     def rewriting_rules
       rules = super
       rules.map do |expression, rule|
