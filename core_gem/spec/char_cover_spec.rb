f4c153fb740c72461335bcbde70bf8065bcd0570
Marc-Andre Lafortune <github@marc-andre.ca>
2018-08-29 16:20:24 -0400

    Fix path for char_cover specs

--- core_gem/spec/char_cover_spec.rb
+++ core_gem/spec/char_cover_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe 'char cover' do
 
   it 'tests against at least one of every node types', :pending, :slow do
     visited = Set.new
-    Dir.glob('./spec/char_cover/*.rb') do |filename|
+    Dir.glob("#{__dir__}/char_cover/*.rb") do |filename|
       ast = DeepCover::CoveredCode.new(path: filename).covered_ast
       ast.each_node do |node|
         visited << node.class
