3fbf2bca6b426b3e56d4d9a32592eeb7ca141550
Marc-Andre Lafortune <github@marc-andre.ca>
2018-08-29 14:31:29 -0400

    Ignore uncovered 'is_statement' methods, at least for now

--- .deep_cover.rb
+++ .deep_cover.rb
@@ -5,4 +5,9 @@
 DeepCover.configure do
   tracker_global '$_dcg'
   ignore_uncovered :warn, :raise, :default_argument
+  # Statement analyser is only used for Istanbul output and is undertested.
+  # Ignore for now. TODO: test this, or get rid of StatementAnalyser
+  ignore_uncovered :is_statement_methods do
+    parent.is_a?(DeepCover::Node::Def) && parent.method_name == :is_statement
+  end
 end
