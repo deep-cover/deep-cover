eaa688de36fe31e77d9b5ce1bf0f2b357c0ba891
Marc-Andre Lafortune <github@marc-andre.ca>
2018-08-29 15:12:46 -0400

    Simplify.
    Body can never be nil, it is mapped to an EmptyBody.
    node/loops now 100% covered

--- core_gem/lib/deep_cover/node/loops.rb
+++ core_gem/lib/deep_cover/node/loops.rb
@@ -4,7 +4,7 @@ module DeepCover
   class Node
     class For < Node
       has_tracker :body
-      has_child assignments: [Mlhs, VariableAssignment], flow_entry_count: -> { body.flow_entry_count if body }
+      has_child assignments: [Mlhs, VariableAssignment], flow_entry_count: -> { body.flow_entry_count }
       has_child iterable: [Node], flow_entry_count: -> { flow_entry_count }
       has_child body: Node,
                 can_be_empty: -> { base_node.loc.end.begin },
