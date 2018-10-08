c25e883a5111b9021cc79d9f9ceb8cb5e8aee24f
Maxime Lapointe <hunter_spawn@hotmail.com>
2018-10-08 12:57:12 -0400

    Fix name of a Tool

--- core_gem/lib/deep_cover/tools/scan_match_datas.rb
+++ core_gem/lib/deep_cover/tools/scan_match_datas.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module DeepCover
-  module Tools::RequireRelativeDir
+  module Tools::ScanMatchDatas
     # Like String#scan, but return the MatchData object instead
     def scan_match_datas(source, matcher)
       source.to_enum(:scan, matcher).map { Regexp.last_match }
