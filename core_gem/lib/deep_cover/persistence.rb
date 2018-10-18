28b2021d0324baf963630ceee4430aeafee7020d
Maxime Lapointe <hunter_spawn@hotmail.com>
2018-10-18 09:15:33 -0400

    Correctly merge tracker_hits by summing the hits for each path

--- core_gem/lib/deep_cover/persistence.rb
+++ core_gem/lib/deep_cover/persistence.rb
@@ -27,17 +27,45 @@ module DeepCover
 
     # returns a TrackerHitsPerPath
     def load_trackers
-      tracker_files.map do |full_path|
+      tracker_hits_per_path_hashes = tracker_files.map do |full_path|
         Marshal.load(full_path.binread).yield_self do |version:, tracker_hits_per_path:| # rubocop:disable Security/MarshalLoad
           raise "dump version mismatch: #{version}, currently #{DeepCover::VERSION}" unless version == DeepCover::VERSION
           tracker_hits_per_path
         end
-      end.inject(:merge!)
+      end
+
+      self.class.merge_tracker_hits_per_paths(*tracker_hits_per_path_hashes)
     end
 
 
     def delete_trackers
       tracker_files.each(&:delete)
+    end
+
+    def self.merge_tracker_hits_per_paths(*tracker_hits_per_path_hashes)
+      return {} if tracker_hits_per_path_hashes.empty?
+
+      result = tracker_hits_per_path_hashes[0].transform_values(&:dup)
+
+      tracker_hits_per_path_hashes[1..-1].each do |tracker_hits_per_path|
+        tracker_hits_per_path.each do |path, tracker_hits|
+          matching_result = result[path]
+          if matching_result.nil?
+            result[path] = tracker_hits.dup
+            next
+          end
+
+          if matching_result.size != tracker_hits.size
+            raise "Attempting to merge trackers of different sizes: #{matching_result.size} vs #{tracker_hits.size}, for path #{path}"
+          end
+
+          tracker_hits.each_with_index do |nb_hits, i|
+            matching_result[i] += nb_hits
+          end
+        end
+      end
+
+      result
     end
 
     private
