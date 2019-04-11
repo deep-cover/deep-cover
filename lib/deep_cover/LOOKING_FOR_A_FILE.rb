8d7f97fb33b7a2d7d70e5d94ee99f6e2e9a66f5e
Marc-Andre Lafortune <github@marc-andre.ca>
2019-04-11 15:34:51 -0400

    Rubofix

--- lib/deep_cover/LOOKING_FOR_A_FILE.rb
+++ lib/deep_cover/LOOKING_FOR_A_FILE.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # If you are looking for a file here that is missing
 # it's probably in the `deep-cover-core` gem or
 # `core_gem/lib/deep_cover_core` if you have the full repo
