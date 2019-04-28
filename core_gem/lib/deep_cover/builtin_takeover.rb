d83c0b9f8d3dd59a28cdf81cf3ea1848eac77dab
Maxime Lapointe <hunter_spawn@hotmail.com>
2019-04-28 11:44:47 -0400

    Raise when taking over if the builtin coverage is running

--- core_gem/lib/deep_cover/builtin_takeover.rb
+++ core_gem/lib/deep_cover/builtin_takeover.rb
@@ -5,6 +5,8 @@ require_relative 'coverage'
 require_relative 'core_ext/coverage_replacement'
 
 require 'coverage'
+raise "Ruby's builtin coverage is already running, cannot do a takeover" if Coverage.respond_to?(:running?) && Coverage.running?
+
 BuiltinCoverage = Coverage
 Object.send(:remove_const, 'Coverage')
 Coverage = DeepCover::CoverageReplacement.dup
