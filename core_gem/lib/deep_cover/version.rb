876f176fd04f4dc91beddd42730c49e213210a05
Marc-Andre Lafortune <github@marc-andre.ca>
2020-04-12 02:16:26 -0400

    v0.7.9

--- core_gem/lib/deep_cover/version.rb
+++ core_gem/lib/deep_cover/version.rb
@@ -3,5 +3,5 @@
 top_level_module = Thread.current['_deep_cover_top_level_module'] || Object
 
 module top_level_module::DeepCover # rubocop:disable Naming/ClassAndModuleCamelCase
-  VERSION = '0.7.8'
+  VERSION = '0.7.9'
 end
