741c03736c083c491fb865e97fcca396452fe7ce
Marc-Andre Lafortune <github@marc-andre.ca>
2020-06-01 13:40:44 -0400

    Accept STDIN for deep-cover run-expression

--- lib/deep_cover/cli/commands/run_expression.rb
+++ lib/deep_cover/cli/commands/run_expression.rb
@@ -2,11 +2,12 @@
 
 module DeepCover
   class CLI
-    desc "run-expression [OPTIONS] 'ruby code'", 'Show coverage results for the given ruby expression'
+    desc "run-expression [OPTIONS] ['ruby code']", 'Show coverage results for the given ruby expression (or STDIN)'
     option '--profile', desc: 'use profiler', type: :boolean if RUBY_PLATFORM != 'java'
     option '--debug', aliases: '-d', desc: 'opens an interactive console for debugging', type: :boolean
-    def run_expression(expression)
+    def run_expression(expression = nil)
       require_relative '../../expression_debugger'
+      expression ||= STDIN.read.tap { STDIN.reopen('/dev/tty') }
       ExpressionDebugger.new(expression, **options.transform_keys(&:to_sym)).show
     end
   end
