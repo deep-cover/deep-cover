06a83f72b9308998008727b09665703764608aec
Maxime Lapointe <hunter_spawn@hotmail.com>
2020-06-04 15:41:55 -0400

    Fix another warning in more recent Ruby versions

--- core_gem/lib/deep_cover/tools/struct_with_options.rb
+++ core_gem/lib/deep_cover/tools/struct_with_options.rb
@@ -11,6 +11,9 @@ module DeepCover
     def self.new(*args)
       Struct.new(*args, :options).tap do |klass|
         klass.include Initializer
+        class << klass
+          undef_method :new
+        end
         klass.define_singleton_method(:new, Class.method(:new))
       end
     end
