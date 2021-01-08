699717bb50741e22859c012f81c8419569a0b3b4
John Mortlock <john.mortlock@gmail.com>
2021-01-08 17:57:25 +1030

    Add minimum coverage check (#73)
    Co-authored-by: John Mortlock <john.mortlock@sealink.com.au>

--- lib/deep_cover/cli/commands/report.rb
+++ lib/deep_cover/cli/commands/report.rb
@@ -6,9 +6,17 @@ module DeepCover
     option '--output', desc: 'output folder', type: :string, default: DeepCover.config.output, aliases: '-o'
     option '--reporter', desc: 'reporter to use', type: :string, default: DeepCover.config.reporter
     option '--open', desc: 'open the output coverage', type: :boolean, default: CLI_DEFAULTS[:open]
+    option '--minimum-coverage', desc: 'minimum coverage percent', type: :numeric, default: 0
     def report
       coverage = Coverage.load
       puts coverage.report(**processed_options)
+
+      overall_coverage = coverage.analysis.overall
+      minimum_coverage = processed_options.fetch('minimum-coverage'.to_sym, 0).to_f
+      if overall_coverage < minimum_coverage
+        puts "Overall coverage #{format('%.2f', overall_coverage)} is less than minimum #{format('%.2f', minimum_coverage)}"
+        exit 1
+      end
     end
   end
 end
